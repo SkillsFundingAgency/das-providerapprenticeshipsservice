efdb6b6d56e336a27e548c496409dd0874dd5f89
ijrussell <i.j.russell@tesco.net>
2016-09-16 15:45:22 +0100

    Added provider, commitment api client and task api client projects plus added cloud service project

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Domain/User.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.ProviderApprenticeshipsService.Domain
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserRef { get; set; }
+        public long ProviderId { get; set; }
+    }
+}
