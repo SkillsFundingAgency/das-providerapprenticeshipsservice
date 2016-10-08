85e051ef5941a232d58d5b80996fb9f1f7435585
Lee Wadhams <lee.wadhams@valtech.com>
2016-10-08 10:53:32 +0100

    Added StorageConnection variable to Cloud Service

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Global.asax.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Global.asax.cs
@@ -1,17 +1,22 @@
 ﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Logger.Info("Starting up");
         }
     }
 }
