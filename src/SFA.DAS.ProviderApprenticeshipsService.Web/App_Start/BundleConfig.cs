ca1e0f8bfe7a37243d291b7cd7f4113f599dbad3
RajeshV <rajesh@digi2al.co.uk>
2016-11-10 11:32:22 +0000

    Sass implemented, layout file changed to reflect style and script changes in bundles.

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/BundleConfig.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/BundleConfig.cs
@@ -8,27 +8,17 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/sfajs").Include(
+                              "~/dist/javascripts/jquery-1.11.0.min.js",
+                              "~/dist/javascripts/govuk-template.js",
+                              "~/dist/javascripts/selection-buttons.js",
+                              "~/dist/javascripts/app.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryvalcustom").Include(
-                        "~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.unobtrusive.custom.js"));
+                      "~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.unobtrusive.custom.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/elements.css",
-                "~/Content/site.css"));
         }
     }
 }
