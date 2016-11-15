c8e3ecc7eb48e86754941ef9e48eff7f4f9a33bd
Mark Gwilliam <github@markgwilliam.com>
2016-11-15 11:09:59 +0000

    added provider idams integration, removed old fake user page, added privacy and terms pages

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/AccountHomeViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models
+{
+    public class AccountHomeViewModel
+    {
+        public string ProviderId { get; set; }
+        public string ProviderName { get; set; }
+    }
+}
