845be3f83684a5373abf2f6041abdeacfdedb96f
Mark Gwilliam <github@markgwilliam.com>
2016-11-22 07:48:47 +0000

    added todo

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/CommitmentListViewModel.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/CommitmentListViewModel.cs
@@ -3,6 +3,7 @@ using SFA.DAS.Commitments.Api.Types;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models
 {
+    //todo: change this type to incorporate status, allowed action, etc. (don't use the API type as a view model)
     public class CommitmentListViewModel
     {
         public long ProviderId { get; set; }
