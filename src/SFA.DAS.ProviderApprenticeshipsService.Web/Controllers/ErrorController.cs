3a29dc6f16642e5f435baaeb3b371c93366c3b74
froberg <christofer.froberg@gmail.com>
2016-11-23 12:39:25 +0000

    Added error pages

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/ErrorController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Controllers
+{
+    using System;
+
+    public class ErrorController : Controller
+    {
+        public ViewResult BadRequest()
+        {
+            Response.StatusCode = 400;
+
+            return View("_Error400");
+        }
+
+        public ViewResult NotFound()
+        {
+            Response.StatusCode = 404;
+
+            return View("_Error404");
+        }
+
+        public ViewResult InternalServerError(Exception ex)
+        {
+            Response.StatusCode = 500;
+            return View("_Error500");
+        }
+    }
+}
