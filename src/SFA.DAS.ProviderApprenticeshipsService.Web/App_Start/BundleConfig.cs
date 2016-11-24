7a3a19fb48548b5074ed7b74f6931e55d44d02c3
Rajesh Veluchamy <velurajesh@gmail.com>
2016-11-24 15:38:17 +0000

    Provider ui changes  (#45)
    * Bundles & Layout updated with styles referenced.
    
    * select dropdown css file renamed and layout page updated
    
    * UI and content changes for Wave2b
    
    * style changes for select box focus
    
    * class form-hint updated
    
    * bundle link updated for css

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/BundleConfig.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/BundleConfig.cs
@@ -23,6 +23,13 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryvalcustom").Include(
                       "~/Scripts/jquery.validate.js", "~/Scripts/jquery.validate.unobtrusive.custom.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/dropdown-select").Include("~/dist/css/dropdown-select.min.css"));
+            bundles.Add(new StyleBundle("~/bundles/screenie6").Include("~/dist/css/screen-ie6.css"));
+            bundles.Add(new StyleBundle("~/bundles/screenie7").Include("~/dist/css/screen-ie7.css"));
+            bundles.Add(new StyleBundle("~/bundles/screenie8").Include("~/dist/css/screen-ie8.css"));
+           
+            bundles.Add(new StyleBundle("~/bundles/screen").Include("~/dist/css/screen.css"));
         }
     }
 }
--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
@@ -15,6 +15,7 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
                 .NotEmpty().WithMessage("Please enter a valid unique learner number")
                 .Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter a valid unique learner number");
             RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the total agreed cost");
+
             RuleFor(x => x.StartMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid start month for training");
             RuleFor(x => x.StartYear).InclusiveBetween(currentYear, 9999).WithMessage("Please enter a valid start year for training");
             RuleFor(x => x.EndMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid finish month for training");
@@ -22,17 +23,18 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
 
             RuleFor(x => x.NINumber)
                 .Matches(@"^[abceghj-prstw-z][abceghj-nprstw-z]\d{6}[abcd]$", RegexOptions.IgnoreCase)
-                .WithMessage("Please enter a valid national insurance number.");
+                .WithMessage("Please enter a valid national insurance number");
 
             RuleFor(x => x.DateOfBirthDay)
                 .InclusiveBetween(1, 31)
-                .WithMessage("Please enter a valid day for date of birth.");
+                .WithMessage("Please enter a valid day for date of birth");
             RuleFor(x => x.DateOfBirthMonth)
                 .InclusiveBetween(1, 12)
-                .WithMessage("Please enter a valid month for date of birth.");
+                .WithMessage("Please enter a valid month for date of birth");
             RuleFor(x => x.DateOfBirthYear)
                 .InclusiveBetween(1900, currentYear)
                 .WithMessage("Please enter a valid year for date of birth");
+            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the total agreed cost");
         }
     }
 }
