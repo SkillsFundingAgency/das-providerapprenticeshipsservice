f19e90a71ef9dfaffa359395afa6080ff2acecd7
RajeshV <rajesh@digi2al.co.uk>
2016-11-24 11:53:07 +0000

    UI and content changes for Wave2b

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
@@ -12,7 +12,7 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
             var currentYear = System.DateTime.Now.Year;
 
             RuleFor(x => x.ULN).Matches("^$|^[1-9]{1}[0-9]{9}$").WithMessage("Please enter a valid unique learner number");
-            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the total agreed cost");
+           
             RuleFor(x => x.StartMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid start month for training");
             RuleFor(x => x.StartYear).InclusiveBetween(currentYear, 9999).WithMessage("Please enter a valid start year for training");
             RuleFor(x => x.EndMonth).InclusiveBetween(1, 12).WithMessage("Please enter a valid finish month for training");
@@ -20,17 +20,18 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
 
             RuleFor(x => x.NINumber)
                 .Matches(@"^[abceghj-prstw-z][abceghj-nprstw-z]\d{6}[abcd]$", RegexOptions.IgnoreCase)
-                .WithMessage("Please enter a valid national insurance number.");
+                .WithMessage("Please enter a valid national insurance number");
 
             RuleFor(x => x.DateOfBirthDay)
                 .InclusiveBetween(1, 31)
-                .WithMessage("Please enter a valid day for date of birth.");
+                .WithMessage("Please enter a valid day for date of birth");
             RuleFor(x => x.DateOfBirthMonth)
                 .InclusiveBetween(1, 12)
-                .WithMessage("Please enter a valid month for date of birth.");
+                .WithMessage("Please enter a valid month for date of birth");
             RuleFor(x => x.DateOfBirthYear)
                 .InclusiveBetween(1900, currentYear)
                 .WithMessage("Please enter a valid year for date of birth");
+            RuleFor(x => x.Cost).Matches("^$|^[1-9]{1}[0-9]*$").WithMessage("Please enter the total agreed cost");
         }
     }
 }
