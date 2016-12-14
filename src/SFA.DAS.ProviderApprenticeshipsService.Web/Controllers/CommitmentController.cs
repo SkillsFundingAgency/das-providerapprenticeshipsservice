8d06d4d99ce14872af63fee42632146c0e3164ea
froberg <christofer.froberg@gmail.com>
2016-12-14 11:04:18 +0000

    Added empty pages for bulk upload

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 using SFA.DAS.ProviderApprenticeshipsService.Application;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models;
@@ -187,6 +188,42 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Controllers
                 ProviderName = commitment.ProviderName,
                 Message = message
             });
+        }
+
+        [HttpGet]
+        [Route("{hashedCommitmentId}/UploadApprenticeships")]
+        public ActionResult UploadApprenticeships(long providerid, string hashedcommitmentid)
+        {
+            var model = new UploadApprenticeshipsViewModel
+                            {
+                                ProviderId = providerid,
+                                HashedCommitmentId = hashedcommitmentid
+                            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("{hashedCommitmentId}/UploadApprenticeships")]
+        public ActionResult UploadApprenticeships(UploadApprenticeshipsViewModel uploadApprenticeshipsViewModel, [Bind(Include = "Attachment")]HttpPostedFileBase upload)
+        {
+            // ToDo: sent to orchestrator
+            // branch on returning model error count
+            if (uploadApprenticeshipsViewModel.Attachment != null)
+            {
+                
+                // ToDo: Flash message, or other feedback to customer
+                return RedirectToAction("Details", new { uploadApprenticeshipsViewModel.ProviderId, uploadApprenticeshipsViewModel.HashedCommitmentId});
+            }
+
+            // ToDo: Need to return errors
+            return RedirectToAction("UploadApprenticeshipsUnsuccessful", uploadApprenticeshipsViewModel);
+        }
+
+
+        [Route("{hashedCommitmentId}/UploadApprenticeships/Unsuccessful")]
+        public ActionResult UploadApprenticeshipsUnsuccessful(long providerid, string hashedcommitmentid)
+        {
+            return View();
         }
 
         private void AddErrorsToModelState(InvalidRequestException ex)
