9663f779d352f625bc1b88d0bff7e05bf3b8a911
froberg <christofer.froberg@gmail.com>
2016-12-14 11:21:32 +0000

    Moved bulk upload to different controller

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/BulkUploadController.cs
@@ -0,0 +1,46 @@
+﻿using System.Web.Mvc;
+
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Controllers
+{
+    [RoutePrefix("{providerId}/apprentices")]
+    public class BulkUploadController : Controller
+    {
+        [HttpGet]
+        [Route("{hashedCommitmentId}/UploadApprenticeships")]
+        public ActionResult UploadApprenticeships(long providerid, string hashedcommitmentid)
+        {
+            var model = new UploadApprenticeshipsViewModel
+            {
+                ProviderId = providerid,
+                HashedCommitmentId = hashedcommitmentid
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [Route("{hashedCommitmentId}/UploadApprenticeships")]
+        public ActionResult UploadApprenticeships(UploadApprenticeshipsViewModel uploadApprenticeshipsViewModel)
+        {
+            // ToDo: sent to orchestrator
+            // branch on returning model error count
+            if (uploadApprenticeshipsViewModel.Attachment != null)
+            {
+
+                // ToDo: Flash message, or other feedback to customer
+                return RedirectToAction("Details", "Commitment", new { uploadApprenticeshipsViewModel.ProviderId, uploadApprenticeshipsViewModel.HashedCommitmentId });
+            }
+
+            // ToDo: Need to return errors
+            return RedirectToAction("UploadApprenticeshipsUnsuccessful", uploadApprenticeshipsViewModel);
+        }
+
+
+        [Route("{hashedCommitmentId}/UploadApprenticeships/Unsuccessful")]
+        public ActionResult UploadApprenticeshipsUnsuccessful(long providerid, string hashedcommitmentid)
+        {
+            return View();
+        }
+    }
+}
--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
@@ -190,42 +190,6 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Controllers
             });
         }
 
-        [HttpGet]
-        [Route("{hashedCommitmentId}/UploadApprenticeships")]
-        public ActionResult UploadApprenticeships(long providerid, string hashedcommitmentid)
-        {
-            var model = new UploadApprenticeshipsViewModel
-                            {
-                                ProviderId = providerid,
-                                HashedCommitmentId = hashedcommitmentid
-                            };
-            return View(model);
-        }
-
-        [HttpPost]
-        [Route("{hashedCommitmentId}/UploadApprenticeships")]
-        public ActionResult UploadApprenticeships(UploadApprenticeshipsViewModel uploadApprenticeshipsViewModel, [Bind(Include = "Attachment")]HttpPostedFileBase upload)
-        {
-            // ToDo: sent to orchestrator
-            // branch on returning model error count
-            if (uploadApprenticeshipsViewModel.Attachment != null)
-            {
-                
-                // ToDo: Flash message, or other feedback to customer
-                return RedirectToAction("Details", new { uploadApprenticeshipsViewModel.ProviderId, uploadApprenticeshipsViewModel.HashedCommitmentId});
-            }
-
-            // ToDo: Need to return errors
-            return RedirectToAction("UploadApprenticeshipsUnsuccessful", uploadApprenticeshipsViewModel);
-        }
-
-
-        [Route("{hashedCommitmentId}/UploadApprenticeships/Unsuccessful")]
-        public ActionResult UploadApprenticeshipsUnsuccessful(long providerid, string hashedcommitmentid)
-        {
-            return View();
-        }
-
         private void AddErrorsToModelState(InvalidRequestException ex)
         {
             foreach (var error in ex.ErrorMessages)
