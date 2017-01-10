09315a7e889eb8d3964c1cea0d57a59afb77893a
Lee Wadhams <lee.wadhams@valtech.com>
2017-01-10 09:23:44 +0000

    Correct error message and rename method for clarity

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/ApprenticeshipViewModelValidator.cs
@@ -15,7 +15,7 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
             var yesterday = DateTime.Now.AddDays(-1);
             var text = new ApprenticeshipValidationText();
             Func<string, int, bool> lengthLessThan = (str, length) => (str?.Length ?? 0) <= length;
-            Func<string, int, bool> numberOfDigitsLessThan = (str, length) => { return (str?.Count(char.IsDigit) ?? 0) < length; };
+            Func<string, int, bool> haveNumberOfDigitsFewerThan = (str, length) => { return (str?.Count(char.IsDigit) ?? 0) < length; };
             Func<DateTime?, bool, bool> _checkIfNotNull = (dt, b) => dt == null || b;
 
             RuleFor(x => x.FirstName)
@@ -27,8 +27,8 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
                 .Must(m => lengthLessThan(m, 100)).WithMessage("You must enter a last name that's no longer than 100 characters");
 
             RuleFor(x => x.Cost)
-                .Matches("^$|^([1-9]{1}([0-9]{1,2})?)+(,[0-9]{3})*$").When(m => numberOfDigitsLessThan(m.Cost, 7)).WithMessage("Enter the total agreed training cost")
-                .Must(m => numberOfDigitsLessThan(m, 7)).WithMessage("The cost must be 6 numbers or fewer, for example 25000");
+                .Matches("^$|^([1-9]{1}([0-9]{1,2})?)+(,[0-9]{3})*$").When(m => haveNumberOfDigitsFewerThan(m.Cost, 7)).WithMessage("Enter the total agreed training cost")
+                .Must(m => haveNumberOfDigitsFewerThan(m, 7)).WithMessage("The cost must be 6 numbers or fewer, for example 25000");
 
             RuleFor(r => r.StartDate)
                 .Must(ValidateDateWithoutDay).Unless(m => m.StartDate == null).WithMessage("The Learning start end date is not valid");
