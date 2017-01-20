262a672c19c0f1a0586cc3f5a916e59332f1a4ef
froberg <christofer.froberg@gmail.com>
2017-01-20 14:01:16 +0000

    Added test for GetCohorts

--- src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/WhenGetCohorts.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/WhenGetCohorts.cs
@@ -20,20 +20,37 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators
     public class WhenGetCohorts
     {
         [Test]
+        public async Task TestHappyPath()
+        {
+            var mockMediator = new Mock<IMediator>();
+            var mockCalculator = new Mock<ICommitmentStatusCalculator>();
+            var commitments = Task.FromResult(TestData());
+            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetCommitmentsQueryRequest>())).Returns(commitments);
+
+            var sut = new CommitmentOrchestrator(mockMediator.Object, mockCalculator.Object, Mock.Of<IHashingService>(), Mock.Of<IProviderCommitmentsLogger>());
+
+            await sut.GetCohorts(1234567);
+
+            mockMediator.Verify(m => m.SendAsync(It.IsAny<GetCommitmentsQueryRequest>()), Times.Once);
+            mockCalculator.Verify(m => m.GetStatus(It.IsAny<EditStatus>(), It.IsAny<int>(), It.IsAny<LastAction>(), It.IsAny<AgreementStatus>()), Times.Exactly(commitments.Result.Commitments.Count));
+        }
+
+        [Test]
         public async Task TestFilter()
         {
             var mockMediator = new Mock<IMediator>();
             var commitments = Task.FromResult(TestData());
             mockMediator.Setup(m => m.SendAsync(It.IsAny<GetCommitmentsQueryRequest>())).Returns(commitments);
-            // ICommitmentStatusCalculator
-            var mockCalculator = new Mock<ICommitmentStatusCalculator>();
 
             var sut = new CommitmentOrchestrator(mockMediator.Object, new CommitmentStatusCalculator(), Mock.Of<IHashingService>(), Mock.Of<IProviderCommitmentsLogger>());
 
             var result = await sut.GetCohorts(1234567);
 
-            result.NewRequestsCount.Should().Be(0);
+            mockMediator.Verify(m => m.SendAsync(It.IsAny<GetCommitmentsQueryRequest>()), Times.Once);
 
+            result.NewRequestsCount.Should().Be(1);
+            result.ReadyForApprovalCount.Should().Be(2);
+            result.WithEmployerCount.Should().Be(2);
         }
 
         private static GetCommitmentsQueryResponse TestData()
@@ -42,10 +59,30 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators
                        {
                            Commitments = new List<CommitmentListItem>
                                              {
-                                                 new CommitmentListItem
+                                                new CommitmentListItem // NewRequest
+                                                    { AgreementStatus = AgreementStatus.NotAgreed, ApprenticeshipCount = 0,
+                                                      CanBeApproved = true, CommitmentStatus = CommitmentStatus.Active,
+                                                      EditStatus = EditStatus.ProviderOnly, LastAction = LastAction.Amend
+                                                    },
+                                                 new CommitmentListItem // ReadyForApproval
                                                     { AgreementStatus = AgreementStatus.EmployerAgreed, ApprenticeshipCount = 5,
                                                       CanBeApproved = true, CommitmentStatus = CommitmentStatus.Active,
                                                       EditStatus = EditStatus.ProviderOnly, LastAction = LastAction.Approve
+                                                    },
+                                                 new CommitmentListItem // ReadyForApproval
+                                                    { AgreementStatus = AgreementStatus.EmployerAgreed, ApprenticeshipCount = 5,
+                                                      CanBeApproved = true, CommitmentStatus = CommitmentStatus.Active,
+                                                      EditStatus = EditStatus.ProviderOnly, LastAction = LastAction.Approve
+                                                    },
+                                                 new CommitmentListItem // With employer
+                                                    { AgreementStatus = AgreementStatus.NotAgreed, ApprenticeshipCount = 6,
+                                                      CanBeApproved = true, CommitmentStatus = CommitmentStatus.Active,
+                                                      EditStatus = EditStatus.EmployerOnly, LastAction = LastAction.Amend
+                                                    },
+                                                 new CommitmentListItem // With employer
+                                                    { AgreementStatus = AgreementStatus.ProviderAgreed, ApprenticeshipCount = 6,
+                                                      CanBeApproved = true, CommitmentStatus = CommitmentStatus.Active,
+                                                      EditStatus = EditStatus.EmployerOnly, LastAction = LastAction.Approve
                                                     }
                                              }
                        };
