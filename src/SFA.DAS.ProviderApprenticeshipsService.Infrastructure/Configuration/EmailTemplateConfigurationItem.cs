beaabaf49b4db1b5aef10ec375aec211397c974d
froberg <christofer.froberg@gmail.com>
2017-02-07 10:13:49 +0000

    Removed unused property

--- src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Configuration/EmailTemplateConfigurationItem.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Configuration/EmailTemplateConfigurationItem.cs
@@ -3,17 +3,11 @@ using Newtonsoft.Json.Converters;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Configuration
 {
-    public enum EmailTemplateType
-    {
-        None,
-        CommitmentNotification
-    }
-
     public class EmailTemplateConfigurationItem
     {
         [JsonConverter(typeof(StringEnumConverter))]
         public EmailTemplateType TemplateType { get; set; }
-        public string TemplateName { get; set; }
+
         public string Key { get; set; }
     }
 }
