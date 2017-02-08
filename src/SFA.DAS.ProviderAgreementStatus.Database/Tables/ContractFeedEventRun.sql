867fbaf3708e0186134d5d76bcb06b7934eb7d06
froberg <christofer.froberg@gmail.com>
2017-02-08 09:07:17 +0000

    DCM-239 -> added new table and config

---
+++ src/SFA.DAS.ProviderAgreementStatus.Database/Tables/ContractFeedEventRun.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[ContractFeedEventRun]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[EntriesSaved] Int NOT NULL,
+	[PageNumber] Int NOT NULL,
+	[Updated] DateTime NOT NULL
+)
---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Domain/ContractFeed/EventRun.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.ProviderApprenticeshipsService.Domain.ContractFeed
+{
+    public class EventRun
+    {
+        public int NewLatestPageNumber { get; set; }
+
+        public int ContractCount { get; set; }
+    }
+}
