20736745b3c960bddb0927d94eae67aba6816912
Christopher Foster <Christopher Foster>
2017-03-08 14:59:31 +0000

    Replaces IsNullOrEmpty with IsNullOrWhiteSpace

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/CsvRecordValidator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Validation/CsvRecordValidator.cs
@@ -13,7 +13,7 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
     public class CsvRecordValidator : AbstractValidator<CsvRecord>
     {
         private static Func<string, IEnumerable<string>, bool> _inList = (v, l) 
-            => string.IsNullOrEmpty(v) || l.Contains(v);
+            => string.IsNullOrWhiteSpace(v) || l.Contains(v);
 
         public CsvRecordValidator(BulkUploadApprenticeshipValidationText validationText)
         {
@@ -27,38 +27,38 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Validation
 
             RuleFor(r => r.FworkCode)
                 .Cascade(CascadeMode.StopOnFirstFailure)
-                .Must(m => m.TryParse() <= 999).When(m => !string.IsNullOrEmpty(m.FworkCode))
+                .Must(m => m.TryParse() <= 999).When(m => !string.IsNullOrWhiteSpace(m.FworkCode))
                     .WithMessage(validationText.FworkCode01.Text).WithErrorCode(validationText.FworkCode01.ErrorCode)
-                .Must(m => (m.TryParse() ?? 0) > 0).When(m => !string.IsNullOrEmpty(m.ProgType) && _inList(m.ProgType, new[] { "2", "3", "20", "21", "22", "23" })).WithMessage(validationText.FworkCode02.Text).WithErrorCode(validationText.FworkCode02.ErrorCode);
+                .Must(m => (m.TryParse() ?? 0) > 0).When(m => !string.IsNullOrWhiteSpace(m.ProgType) && _inList(m.ProgType, new[] { "2", "3", "20", "21", "22", "23" })).WithMessage(validationText.FworkCode02.Text).WithErrorCode(validationText.FworkCode02.ErrorCode);
 
             RuleFor(r => r.FworkCode)
-                .Must(m => string.IsNullOrEmpty(m) || m == "0").When(m => m.ProgType == "25").WithMessage(validationText.FworkCode03.Text).WithErrorCode(validationText.FworkCode03.ErrorCode);
+                .Must(m => string.IsNullOrWhiteSpace(m) || m == "0").When(m => m.ProgType == "25").WithMessage(validationText.FworkCode03.Text).WithErrorCode(validationText.FworkCode03.ErrorCode);
 
             RuleFor(r => r.PwayCode)
                 .Cascade(CascadeMode.StopOnFirstFailure)
                 .Must(m => m.TryParse() <= 999)
-                    .When(m => !string.IsNullOrEmpty(m.PwayCode) && _inList(m.ProgType, new[]{ "2","3","20","21","22","23" }))
+                    .When(m => !string.IsNullOrWhiteSpace(m.PwayCode) && _inList(m.ProgType, new[]{ "2","3","20","21","22","23" }))
                     .WithMessage(validationText.PwayCode01.Text).WithErrorCode(validationText.PwayCode01.ErrorCode)
-                .Must(m => (m.TryParse() ?? 0) > 0).When(m => !string.IsNullOrEmpty(m.ProgType) && _inList(m.ProgType, new[] { "2", "3", "20", "21", "22", "23" })).WithMessage(validationText.PwayCode02.Text).WithErrorCode(validationText.PwayCode02.ErrorCode);
+                .Must(m => (m.TryParse() ?? 0) > 0).When(m => !string.IsNullOrWhiteSpace(m.ProgType) && _inList(m.ProgType, new[] { "2", "3", "20", "21", "22", "23" })).WithMessage(validationText.PwayCode02.Text).WithErrorCode(validationText.PwayCode02.ErrorCode);
 
             RuleFor(r => r.PwayCode)
-                .Must(m => string.IsNullOrEmpty(m) || m == "0").When(m => m.ProgType == "25")
+                .Must(m => string.IsNullOrWhiteSpace(m) || m == "0").When(m => m.ProgType == "25")
                     .WithMessage(validationText.PwayCode03.Text).WithErrorCode(validationText.PwayCode03.ErrorCode);
 
             RuleFor(r => r.StdCode)
                 .Cascade(CascadeMode.StopOnFirstFailure)
-                .Must(m => m.TryParse() <= 99999).When(m => !string.IsNullOrEmpty(m.StdCode))
+                .Must(m => m.TryParse() <= 99999).When(m => !string.IsNullOrWhiteSpace(m.StdCode))
                     .WithMessage(validationText.StdCode01.Text).WithErrorCode(validationText.StdCode01.ErrorCode)
                 .Must(m => m.TryParse() > 0).When(m => m.ProgType == "25").WithMessage(validationText.StdCode02.Text).WithErrorCode(validationText.StdCode02.ErrorCode);
 
             RuleFor(r => r.StdCode)
-                .Must(m => string.IsNullOrEmpty(m) || m == "0").When(FrameworkProgTypeSelected)
+                .Must(m => string.IsNullOrWhiteSpace(m) || m == "0").When(FrameworkProgTypeSelected)
                     .WithMessage(validationText.StdCode03.Text).WithErrorCode(validationText.StdCode03.ErrorCode);
         }
 
         private static bool FrameworkProgTypeSelected(CsvRecord record)
         {
-            return !string.IsNullOrEmpty(record.ProgType) && _inList(record.ProgType, new[] { "2","3","20","21","22","23" }) ;
+            return !string.IsNullOrWhiteSpace(record.ProgType) && _inList(record.ProgType, new[] { "2","3","20","21","22","23" }) ;
         }
     }
 }
