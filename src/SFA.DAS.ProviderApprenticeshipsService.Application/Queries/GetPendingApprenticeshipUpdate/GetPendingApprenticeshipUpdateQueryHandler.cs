d9cf0d28dda8b79dcab605b1fe7c217f17570163
Christopher Foster <Christopher Foster>
2017-03-25 08:54:43 +0000

    Adds GetPendingAppUpdate request Updates packages

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application/Queries/GetPendingApprenticeshipUpdate/GetPendingApprenticeshipUpdateQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using MediatR;
+using SFA.DAS.Commitments.Api.Client.Interfaces;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetPendingApprenticeshipUpdate
+{
+    public class GetPendingApprenticeshipUpdateQueryHandler: IAsyncRequestHandler<GetPendingApprenticeshipUpdateQueryRequest,GetPendingApprenticeshipUpdateQueryResponse>
+    {
+        private readonly IProviderCommitmentsApi _apiClient;
+        
+        public GetPendingApprenticeshipUpdateQueryHandler(IProviderCommitmentsApi apiClient)
+        {
+            if(apiClient==null)
+                throw new ArgumentNullException(nameof(apiClient));
+
+            _apiClient = apiClient;
+        }
+
+        public async Task<GetPendingApprenticeshipUpdateQueryResponse> Handle(GetPendingApprenticeshipUpdateQueryRequest message)
+        {
+            var result = await _apiClient.GetPendingApprenticeshipUpdate(message.ProviderId, message.ApprenticeshipId);
+
+            return new GetPendingApprenticeshipUpdateQueryResponse
+            {
+                ApprenticeshipUpdate = result
+            };
+        }
+    }
+}
