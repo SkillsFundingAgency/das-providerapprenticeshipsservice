231629947877262ff4435ead22a36299703a3678
froberg <christofer.froberg@gmail.com>
2017-05-04 09:24:06 +0100

    Removed test data from GetDataLock

--- src/SFA.DAS.ProviderApprenticeshipsService.Application/Queries/GetApprenticeshipDataLock/GetApprenticeshipDataLockHandler.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application/Queries/GetApprenticeshipDataLock/GetApprenticeshipDataLockHandler.cs
@@ -39,57 +39,6 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetApprenti
                 _logger.Warn(ex, $"Can't get apprenticeship data lock for apprenticeship {request.ApprenticeshipId}");
             }
             return new GetApprenticeshipDataLockResponse();
-
-            //return GetTestData(request);
-        }
-
-        private GetApprenticeshipDataLockResponse GetTestData(GetApprenticeshipDataLockRequest request)
-        {
-            DataLockStatus dl = null;
-            if (request.ApprenticeshipId == 495670)
-            {
-                dl = new DataLockStatus
-                {
-                    ApprenticeshipId = request.ApprenticeshipId,
-                    DataLockEventDatetime = DateTime.Now,
-                    DataLockEventId = 123456L,
-                    IlrActualStartDate = DateTime.Now,
-                    IlrEffectiveFromDate = DateTime.Now,
-                    IlrTotalCost = 3000,
-                    IlrTrainingCourseCode = "7",
-                    IlrTrainingType = Commitments.Api.Types.Apprenticeship.Types.TrainingType.Framework,
-                    PriceEpisodeIdentifier = "price-epi",
-                    Status = Status.Fail,
-                    TriageStatus = TriageStatus.Restart,
-                    ErrorCode = DataLockErrorCode.Dlock04 & DataLockErrorCode.Dlock05,
-                };
-
-                // Create new
-            }
-
-            if (request.ApprenticeshipId == 505672)
-            {
-                dl = new DataLockStatus
-                {
-                    ApprenticeshipId = request.ApprenticeshipId,
-                    DataLockEventDatetime = DateTime.Now,
-                    DataLockEventId = 123456L,
-                    IlrActualStartDate = DateTime.Now,
-                    IlrEffectiveFromDate = DateTime.Now,
-                    IlrTotalCost = 3000,
-                    IlrTrainingCourseCode = "8",
-                    IlrTrainingType = Commitments.Api.Types.Apprenticeship.Types.TrainingType.Framework,
-                    PriceEpisodeIdentifier = "price-epi",
-                    Status = Status.Fail,
-                    TriageStatus = TriageStatus.Change,
-                    ErrorCode = DataLockErrorCode.Dlock07 & DataLockErrorCode.Dlock09
-                };
-                // Fix errors
-            }
-
-            var response = new GetApprenticeshipDataLockResponse { Data = dl };
-
-            return response;
         }
     }
 }
