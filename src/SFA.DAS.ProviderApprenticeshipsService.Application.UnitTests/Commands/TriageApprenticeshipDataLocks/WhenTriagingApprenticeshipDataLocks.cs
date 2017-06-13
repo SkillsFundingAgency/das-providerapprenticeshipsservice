745b6da5be900d2f0e6f4671256f081089fd231a
Christopher Foster <Christopher Foster>
2017-06-13 09:06:06 +0100

    Call to triage api

--- src/SFA.DAS.ProviderApprenticeshipsService.Application.UnitTests/Commands/TriageApprenticeshipDataLocks/WhenTriagingApprenticeshipDataLocks.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application.UnitTests/Commands/TriageApprenticeshipDataLocks/WhenTriagingApprenticeshipDataLocks.cs
@@ -1,9 +1,11 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+using MediatR;
 using Moq;
-using NLog;
 using NUnit.Framework;
 using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.Commitments.Api.Types.DataLock;
 using SFA.DAS.Commitments.Api.Types.DataLock.Types;
 using SFA.DAS.NLog.Logger;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Commands.TriageApprenticeshipDataLocks;
@@ -16,16 +18,23 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Application.UnitTests.Commands.
         private TriageApprenticeshipDataLocksCommandHandler _handler;
         private Mock<IDataLockApi> _dataLockApi;
         private Mock<ILog> _logger;
+        private Mock<AbstractValidator<TriageApprenticeshipDataLocksCommand>> _validator;
 
         [SetUp]
         public void Arrange()
         {
             _dataLockApi = new Mock<IDataLockApi>();
-            //_dataLockApi.Setup(x => x.) //todo: set something up here
+            _dataLockApi.Setup(x => x.PatchDataLocks(It.IsAny<long>(),
+                It.IsAny<DataLockTriageSubmission>()))
+                .Returns(() => Task.FromResult(new Unit()));
+
+            _validator = new Mock<AbstractValidator<TriageApprenticeshipDataLocksCommand>>();
+            _validator.Setup(x => x.Validate(It.IsAny<TriageApprenticeshipDataLocksCommand>()))
+                .Returns(() => new ValidationResult());
 
             _logger = new Mock<ILog>();
 
-            _handler = new TriageApprenticeshipDataLocksCommandHandler(_dataLockApi.Object, _logger.Object);
+            _handler = new TriageApprenticeshipDataLocksCommandHandler(_dataLockApi.Object, _logger.Object, _validator.Object);
         }
 
         [Test]
@@ -40,11 +49,29 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Application.UnitTests.Commands.
             };
 
             //Act
-            var result = await _handler.Handle(command);
+            await _handler.Handle(command);
 
             //Assert
-            throw new NotImplementedException();
-            //todo: verify api was called
+            _dataLockApi.Verify(x => x.PatchDataLocks(It.IsAny<long>(),
+                It.IsAny<DataLockTriageSubmission>()), Times.Once());
+        }
+
+        [Test]
+        public async Task ThenTheCommandIsValidated()
+        {
+            //Arrange
+            var command = new TriageApprenticeshipDataLocksCommand
+            {
+                ApprenticeshipId = 1,
+                TriageStatus = TriageStatus.Change,
+                UserId = "USER"
+            };
+
+            //Act
+            await _handler.Handle(command);
+
+            //Assert
+            _validator.Verify(x => x.Validate(It.IsAny<TriageApprenticeshipDataLocksCommand>()), Times.Once);
         }
     }
 }
--- src/SFA.DAS.ProviderApprenticeshipsService.Application/Commands/TriageApprenticeshipDataLocks/TriageApprenticeshipDataLocksCommandHandler.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application/Commands/TriageApprenticeshipDataLocks/TriageApprenticeshipDataLocksCommandHandler.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using FluentValidation;
 using MediatR;
 using SFA.DAS.Commitments.Api.Client.Interfaces;
+using SFA.DAS.Commitments.Api.Types.DataLock;
 using SFA.DAS.NLog.Logger;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Application.Commands.TriageApprenticeshipDataLocks
@@ -8,20 +11,39 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Application.Commands.TriageAppr
     public class TriageApprenticeshipDataLocksCommandHandler : AsyncRequestHandler<TriageApprenticeshipDataLocksCommand>
     {
         private readonly IDataLockApi _dataLockApi;
-
+        private readonly AbstractValidator<TriageApprenticeshipDataLocksCommand> _validator;
         private readonly ILog _logger;
 
-        public TriageApprenticeshipDataLocksCommandHandler(IDataLockApi dataLockApi, ILog logger)
+        public TriageApprenticeshipDataLocksCommandHandler(IDataLockApi dataLockApi, ILog logger, AbstractValidator<TriageApprenticeshipDataLocksCommand> validator)
         {
             _dataLockApi = dataLockApi;
             _logger = logger;
-
-
+            _validator = validator;
         }
 
-        protected override Task HandleCore(TriageApprenticeshipDataLocksCommand message)
+        protected override async Task HandleCore(TriageApprenticeshipDataLocksCommand command)
         {
-            throw new System.NotImplementedException();
+            var validationResult = _validator.Validate(command);
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+
+            var triageSubmission = new DataLockTriageSubmission
+            {
+                TriageStatus = command.TriageStatus,
+                UserId = command.UserId
+            };
+
+            try
+            {
+                await _dataLockApi.PatchDataLocks(command.ApprenticeshipId, triageSubmission);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex, $"Error calling commitment API when updating multiple price data locks to status {command.TriageStatus} for apprenticeship {command.ApprenticeshipId}");
+                throw;
+            }
         }
     }
 }
