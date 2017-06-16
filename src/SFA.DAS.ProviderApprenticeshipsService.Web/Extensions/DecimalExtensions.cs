b4f489073aaa7d05a44442a1cdfde17e56daf2a5
Christopher Foster <Christopher Foster>
2017-06-16 14:58:55 +0100

    Revisiting tables in html

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Extensions/DecimalExtensions.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Extensions/DecimalExtensions.cs
@@ -6,5 +6,10 @@
         {
             return !value.HasValue ? string.Empty : $"£{value.Value:n0}";
         }
+
+        public static string FormatCost(this decimal value)
+        {
+            return $"£{value:n0}";
+        }
     }
 }
