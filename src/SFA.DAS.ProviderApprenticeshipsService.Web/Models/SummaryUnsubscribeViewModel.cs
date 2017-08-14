61d099fc1b18bff1b1ffc12b09bf6947a574054f
froberg <christofer.froberg@gmail.com>
2017-08-14 11:58:28 +0100

    Added unsubscribe page

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/SummaryUnsubscribeViewModel.cs
@@ -0,0 +1,9 @@
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models
+{
+    public class SummaryUnsubscribeViewModel
+    {
+        public bool AlreadyUnsubscribed { get; set; }
+
+        public string AccountName { get; set; }
+    }
+}
