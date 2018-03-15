17701b3e52c71c275707af716ce2ba89cdb12b1a
chrisfoster76 <chrisfoster186@googlemail.com>
2018-03-15 14:15:51 +0000

    Dpp 1176 provider transfer final approver ui notification (#348)
    * Call specific cohort approval method
    
    * Adds Transfer flag to CommitmentView
    Mapping for same
    Unit tests for same
    
    * Adds start date validation rule
    Unit tests
    Markup changes
    
    * Hacked validation render to show alt detail message
    
    * Fixed broken test
    
    * Removes frameworks from transfer-funded cohorts
    
    * Removes phantom js changelog
    
    * Dpp 1099 remove bulk upload for transfers (#344)
    
    * Adds unit tests and implementation for marking commitment as transfer
    
    * Adds unit tests and implementation for disabling bulk upload page for transfers
    
    * Markup change to hide bulk upload button if a transfer
    
    * Minor change to test to make more readable
    
    * Adds unit test to cover existing code
    
    * Split out a new vm from the original
    
    * Adds new orchestrator method for new viewmodel
    
    * Mocked status calculator to finish test
    
    * Markup and controller changes
    
    * Removed comment

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Commitments/WhenGettingApprovedViewModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Types;
+using SFA.DAS.Commitments.Api.Types.Commitment;
+using SFA.DAS.Commitments.Api.Types.Commitment.Types;
+using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitment;
+using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitments;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Commitments
+{
+    [TestFixture]
+    public class WhenGettingApprovedViewModel : ApprenticeshipValidationTestBase
+    {
+        private CommitmentView _commitment;
+        private List<CommitmentListItem> _otherCommitments;
+
+        public override void SetUp()
+        {
+            _commitment = new CommitmentView
+            {
+                Messages = new List<MessageView>()
+            };
+
+            _otherCommitments = new List<CommitmentListItem>();
+
+            _mockMediator.Setup(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()))
+                .ReturnsAsync(() => new GetCommitmentQueryResponse
+                {
+                    Commitment = _commitment
+                });
+
+            _mockMediator.Setup(x => x.SendAsync(It.IsAny<GetCommitmentsQueryRequest>()))
+                .ReturnsAsync(() => new GetCommitmentsQueryResponse
+                {
+                    Commitments = _otherCommitments
+                });
+
+            base.SetUp();
+        }
+
+        [Test]
+        public async Task ThenARequestForTheCommitmentDataIsMade()
+        {
+            await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            _mockMediator.Verify(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()),
+                Times.Once);
+        }
+
+        [Test]
+        public async Task ThenTheViewModelShouldIndicateWhenThereAreOtherCohortsInSameStatus()
+        {
+            _otherCommitments.Add(new CommitmentListItem
+            {
+                ApprenticeshipCount = 10,
+                AgreementStatus = AgreementStatus.EmployerAgreed,
+                CanBeApproved = true,
+                EditStatus = EditStatus.ProviderOnly,
+                LastAction = LastAction.Approve
+            });
+
+            _mockCalculator.Setup(x => x.GetStatus(
+                    It.IsAny<EditStatus>(),
+                    It.IsAny<int>(),
+                    It.IsAny<LastAction>(),
+                    It.IsAny<AgreementStatus>(),
+                    It.IsAny<LastUpdateInfo>()))
+                .Returns(RequestStatus.ReadyForApproval);
+
+            var result = await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            Assert.IsTrue(result.HasOtherCohortsAwaitingApproval);
+        }
+
+        [Test]
+        public async Task ThenTheViewModelShouldIndicateWhenThereAreNotOtherCohortsInSameStatus()
+        {
+            var result = await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            Assert.IsFalse(result.HasOtherCohortsAwaitingApproval);
+        }
+
+        [TestCase(false, "Cohort approved")]
+        [TestCase(true, "Cohort approved and transfer request sent")]
+        public async Task ThenTheHeadlineShouldReflectWhetherTheCommitmentIsToBePaidByTransfer(bool isTransfer, string expectHeadline)
+        {
+            _commitment.TransferSenderId = isTransfer ? 1L : default(long?);
+
+            var result = await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            Assert.AreEqual(expectHeadline, result.Headline);
+        }
+
+        [Test]
+        public async Task ThenTheViewModelShouldIndicateWhenCohortIsForTransfer()
+        {
+            _commitment.TransferSenderId = 1L;
+
+            var result = await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            Assert.IsTrue(result.IsTransfer);
+        }
+
+        [Test]
+        public async Task ThenTheViewModelShouldIndicateWhenCohortIsNotForTransfer()
+        {
+            _commitment.TransferSenderId = null;
+
+            var result = await _orchestrator.GetApprovedViewModel(1, "Hashed-Id");
+
+            Assert.IsFalse(result.IsTransfer);
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/ApprovedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models
+{
+    public class ApprovedViewModel
+    {
+        public string Headline { get; internal set; }
+        public string CommitmentReference { get; internal set; }
+        public string EmployerName { get; internal set; }
+        public string ProviderName { get; internal set; }
+        public bool HasOtherCohortsAwaitingApproval { get; internal set; }
+        public bool IsTransfer { get; internal set; }
+    }
+}
