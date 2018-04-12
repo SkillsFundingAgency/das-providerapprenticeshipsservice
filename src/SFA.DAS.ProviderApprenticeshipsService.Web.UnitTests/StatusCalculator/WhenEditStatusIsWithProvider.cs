bc80d482a1ab65560c806ca4c0fb832821ec9e12
phildavies <github@phildavies.co.uk>
2018-04-12 12:00:18 +0100

    switch status calculator unit test to use extention method on commitmentlistitem

--- src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/StatusCalculator/WhenEditStatusIsWithProvider.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/StatusCalculator/WhenEditStatusIsWithProvider.cs
@@ -1,29 +1,37 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using SFA.DAS.Commitments.Api.Types;
+using SFA.DAS.Commitments.Api.Types.Commitment;
 using SFA.DAS.Commitments.Api.Types.Commitment.Types;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Domain.Commitment;
+using SFA.DAS.ProviderApprenticeshipsService.Application.Extensions;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.StatusCalculator
 {
     [TestFixture]
     public sealed class WhenEditStatusIsWithProvider
     {
-        private static readonly CommitmentStatusCalculator _calculator = new CommitmentStatusCalculator();
-
         [TestCase(RequestStatus.ReadyForReview, LastAction.None, AgreementStatus.NotAgreed, TestName = "Review saved by provider without send")]
         [TestCase(RequestStatus.ReadyForReview, LastAction.Amend, AgreementStatus.NotAgreed, TestName = "Request sent for review with no agreemeent from either party")]
         [TestCase(RequestStatus.ReadyForApproval, LastAction.Approve, AgreementStatus.EmployerAgreed, TestName = "Ready for approval")]
         [TestCase(RequestStatus.ReadyForReview, LastAction.Approve, AgreementStatus.NotAgreed, TestName = "Sent for approval but a change has been made")]
-        public void WhenThereAreApprentices(RequestStatus expectedResult, LastAction lastAction, AgreementStatus overallAgreementStatus)
+        public void WhenThereAreApprentices(RequestStatus expectedResult, LastAction lastAction, AgreementStatus agreementStatus)
         {
-            var lastUpdateInfo = new LastUpdateInfo
+            var commitment = new CommitmentListItem
             {
-                EmailAddress = "test@testcorp",
-                Name = "Test"
+                LastAction = lastAction,
+                AgreementStatus = agreementStatus,
+
+                EditStatus = EditStatus.ProviderOnly,
+                ApprenticeshipCount = 2,
+                ProviderLastUpdateInfo = new LastUpdateInfo
+                {
+                    EmailAddress = "test@testcorp",
+                    Name = "Test"
+                }
             };
 
-            var status = _calculator.GetStatus(EditStatus.ProviderOnly, 2, lastAction, overallAgreementStatus, lastUpdateInfo, null, null);
+            var status = commitment.GetStatus();
 
             status.Should().Be(expectedResult);
         }
@@ -31,10 +39,19 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.StatusCalculator
         [TestCase(LastAction.None, AgreementStatus.NotAgreed)]
         [TestCase(LastAction.Amend, AgreementStatus.NotAgreed)]
         [TestCase(LastAction.Approve, AgreementStatus.EmployerAgreed)]
-        public void WhenTheProviderHasNeverModifiedTheCommitmentItIsClassedAsNew(LastAction lastAction, AgreementStatus overallAgreementStatus)
+        public void WhenTheProviderHasNeverModifiedTheCommitmentItIsClassedAsNew(LastAction lastAction, AgreementStatus agreementStatus)
         {
+            var commitment = new CommitmentListItem
+            {
+                LastAction = lastAction,
+                AgreementStatus = agreementStatus,
+
+                EditStatus = EditStatus.ProviderOnly,
+                ApprenticeshipCount = 2
+            };
+
             //Act
-            var status = _calculator.GetStatus(EditStatus.ProviderOnly, 2, lastAction, overallAgreementStatus, null, null, null);
+            var status = commitment.GetStatus();
 
             //Assert
             Assert.AreEqual(RequestStatus.NewRequest, status);
