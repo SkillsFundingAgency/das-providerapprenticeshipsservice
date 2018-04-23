e6230caade0a9e29ced81b2e44ea53898082a633
Ewan Noble <ewan.noble@education.gov.uk>
2018-04-23 10:20:01 +0100

    Log exceptions on role startup

---
+++ src/SFA.DAS.PAS.Account.Api/WebRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using NLog;
+using System;
+
+namespace SFA.DAS.PAS.Account.Api
+{
+    public class WebRole : RoleEntryPoint
+    {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public override bool OnStart()
+        {
+            try
+            {
+                return base.OnStart();
+            }
+            catch (Exception e)
+            {
+                Logger.Fatal(e);
+                throw e;
+            }
+        }
+}
