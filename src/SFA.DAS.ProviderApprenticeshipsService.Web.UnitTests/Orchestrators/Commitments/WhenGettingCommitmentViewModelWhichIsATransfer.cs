8b3846a460abd5a320dc1287a6628ce5b2fb8e54
Paul Graham <paul.graham@amido.com>
2018-04-23 12:03:56 +0100

    forgot to save this file

--- src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Commitments/WhenGettingCommitmentViewModelWhichIsATransfer.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Commitments/WhenGettingCommitmentViewModelWhichIsATransfer.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 using FluentAssertions;
-using MediatR;
-using Moq;
 using NUnit.Framework;
 using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.Commitments.Api.Types.Apprenticeship;
 using SFA.DAS.Commitments.Api.Types.Commitment;
 using SFA.DAS.Commitments.Api.Types.Commitment.Types;
-using SFA.DAS.Commitments.Api.Types.Validation;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Domain.Commitment;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitment;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetFrameworks;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetOverlappingApprenticeships;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetRelationshipByCommitment;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetStandards;
-using SFA.DAS.ProviderApprenticeshipsService.Domain;
 using CommitmentView = SFA.DAS.Commitments.Api.Types.Commitment.CommitmentView;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Commitments
 {
     using Task = System.Threading.Tasks.Task;
 
-    public class WhenGettingCommitmentViewModelWhichIsATransfer : ApprenticeshipValidationTestBase
+    public class WhenGettingCommitmentViewModelWhichIsATransfer : WhenGettingCommitmentViewModel
     {
         [Test(Description = "Should return ReadOnly if Both Parties have agreed")]
         public async Task ShouldBeAbleToViewACommitmentWhereBothPartiesHaveAgreed()
@@ -71,47 +61,5 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Com
             result.Status.Should().Be(requestStatus);
         }
 
-        // --- Helpers ---
-
-        private static Mock<IMediator> GetMediator(CommitmentView commitment)
-        {
-            var respons = new GetCommitmentQueryResponse
-            {
-                Commitment = commitment
-            };
-
-            var mockMediator = new Mock<IMediator>();
-            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetCommitmentQueryRequest>()))
-                .Returns(Task.Factory.StartNew(() => respons));
-
-            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetStandardsQueryRequest>()))
-                .ReturnsAsync(() => new GetStandardsQueryResponse
-                {
-                    Standards = new List<Standard>()
-                });
-
-            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetFrameworksQueryRequest>()))
-                .ReturnsAsync(() => new GetFrameworksQueryResponse
-                {
-                    Frameworks = new List<Framework>()
-                });
-
-            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetRelationshipByCommitmentQueryRequest>()))
-                .ReturnsAsync(() => new GetRelationshipByCommitmentQueryResponse
-                {
-                    Relationship = new Relationship
-                    {
-                        Verified = true
-                    }
-                });
-
-            mockMediator.Setup(m => m.SendAsync(It.IsAny<GetOverlappingApprenticeshipsQueryRequest>()))
-                .ReturnsAsync(() => new GetOverlappingApprenticeshipsQueryResponse
-                {
-                    Overlaps = new List<ApprenticeshipOverlapValidationResult>()
-                });
-
-            return mockMediator;
-        }
     }
 }
