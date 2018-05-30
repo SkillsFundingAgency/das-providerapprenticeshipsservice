3be7ee79ae697ebef35f8314a800858286dfb66c
phildavies <github@phildavies.co.uk>
2018-05-30 13:06:15 +0100

    first unit test case

--- src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Commitments/WhenGettingAcknowledgementViewModel.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Commitments/WhenGettingAcknowledgementViewModel.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Moq;
 using NUnit.Framework;
+using SFA.DAS.Commitments.Api.Types;
 using SFA.DAS.Commitments.Api.Types.Commitment;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitment;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Types;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Commitments
 {
@@ -31,10 +34,50 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Com
         [Test]
         public async Task ThenARequestForTheCommitmentDataIsMade()
         {
-            await _orchestrator.GetAcknowledgementViewModel(1, "Hashed-Id", Web.Models.Types.SaveStatus.ApproveAndSend);
+            await _orchestrator.GetAcknowledgementViewModel(1, "Hashed-Id", SaveStatus.ApproveAndSend);
 
-            _mockMediator.Verify(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()),
-                Times.Once);
+            _mockMediator.Verify(x => x.SendAsync(It.IsAny<GetCommitmentQueryRequest>()), Times.Once);
+        }
+
+        public enum ExpectedWhatHappensNextType
+        {
+            TransferFirstApproval,
+            EmployerWillReview,
+            UpdatedCohort
+        }
+
+        [TestCase(true, AgreementStatus.ProviderAgreed, SaveStatus.ApproveAndSend, ExpectedWhatHappensNextType.TransferFirstApproval)]
+        public async Task ThenWhatHappensNextIsPopulatedCorrectly(bool isTransfer, AgreementStatus agreementStatus, SaveStatus saveStatus, ExpectedWhatHappensNextType expectedWhatHappensNextType)
+        {
+            if (isTransfer)
+                _commitment.TransferSender = new TransferSender();
+
+            _commitment.AgreementStatus = agreementStatus;
+
+            var viewModel = await _orchestrator.GetAcknowledgementViewModel(1, "Hashed-Id", saveStatus);
+
+            string[] expectedWhatHappensNext;
+            switch (expectedWhatHappensNextType)
+            {
+                case ExpectedWhatHappensNextType.TransferFirstApproval:
+                    expectedWhatHappensNext = new[]
+                    {
+                        "The employer will receive your cohort and will either confirm the information is correct or contact you to suggest changes.",
+                        "Once the employer approves the cohort, a transfer request will be sent to the funding employer to review.",
+                        "You will receive a notification once the funding employer approves or rejects the transfer request. You can view the progress of a request from the 'With transfer sending employers' status screen."
+                    };
+                    break;
+                case ExpectedWhatHappensNextType.EmployerWillReview:
+                    expectedWhatHappensNext = new[] { "The employer will review the cohort and either approve it or contact you with an update." };
+                    break;
+                case ExpectedWhatHappensNextType.UpdatedCohort:
+                    expectedWhatHappensNext = new[] { "The updated cohort will appear in the employer’s account for them to review." };
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            CollectionAssert.AreEquivalent(expectedWhatHappensNext, viewModel.WhatHappensNext);
         }
     }
 }
