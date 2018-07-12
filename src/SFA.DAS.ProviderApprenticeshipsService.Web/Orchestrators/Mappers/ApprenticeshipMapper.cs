2d87e1d47e562b1858208700c70de6a3d9dfcbba
Phil Davies <github@phildavies.co.uk>
2018-07-12 11:39:34 +0100

    fix when end date is actulally editable + improve unit tests around end date editability

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/Mappers/ApprenticeshipMapper.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/Mappers/ApprenticeshipMapper.cs
@@ -66,12 +66,18 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Mappers
                 commitment.TransferSender?.TransferApprovalStatus == TransferApprovalStatus.Approved
                 && !apprenticeship.HasHadDataLockSuccess;
 
-            // if editing post-approval, we disable if start date is in the future and has had data lock success,
-            // as the validation rule that disallows setting end date to > current month
-            // means any date entered would be before the start date (which is also disallowed)
-            var isEndDateLockedForUpdate = commitment.AgreementStatus != AgreementStatus.BothAgreed
-                ? isLockedForUpdate
-                : isLockedForUpdate || (isStartDateInFuture && apprenticeship.HasHadDataLockSuccess);
+            // if editing post-approval, we also lock down end date if...
+            //   start date is in the future and has had data lock success
+            //   (as the validation rule that disallows setting end date to > current month
+            //   means any date entered would be before the start date (which is also disallowed))
+            // and open it up if...
+            //   data lock success and start date in past
+            var isEndDateLockedForUpdate = isLockedForUpdate;
+            if (commitment.AgreementStatus == AgreementStatus.BothAgreed)
+            {
+                if (apprenticeship.HasHadDataLockSuccess)
+                    isEndDateLockedForUpdate = isStartDateInFuture;
+            }
 
             var dateOfBirth = apprenticeship.DateOfBirth;
             return new ApprenticeshipViewModel
