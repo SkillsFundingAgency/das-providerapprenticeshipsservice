b14b67cb8a33df40ee410e7e245f82a87db7755a
Scott Wakefield <scott@expertcc.co.uk>
2018-09-13 14:59:39 +0100

    DPP-1616 added xml formatter, no tests as its deep bespoke code.

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/Formatters/ExcelFormatter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ClosedXML.Excel;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Agreement;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Formatters
+{
+    public class ExcelFormatter : IExcelFormatter
+    {
+        public byte[] Format(IEnumerable<CommitmentAgreement> commitmentAgreements)
+        {
+            var workbook = new XLWorkbook();
+            workbook.AddWorksheet(new AgreementsViewModel {CommitmentAgreements = commitmentAgreements}.ToDataTable(), "Agreements");
+
+            using (var memoryStream = new MemoryStream())
+            {
+                workbook.SaveAs(memoryStream);
+                memoryStream.Seek(0L, SeekOrigin.Begin);
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
