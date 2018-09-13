b1d140ee2634cbe8f87caa6d775b58eae00345dc
Scott Wakefield <scott@expertcc.co.uk>
2018-09-13 10:22:55 +0100

    DPP-1616 orchestrator now returns byte arrays for file downloads.

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AgreementOrchestrator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AgreementOrchestrator.cs
@@ -1,10 +1,12 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using MediatR;
 using SFA.DAS.HashingService;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitmentAgreements;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Agreement;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Formatters;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Mappers;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators
@@ -12,32 +14,55 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators
     public sealed class AgreementOrchestrator : BaseCommitmentOrchestrator
     {
         private readonly IAgreementMapper _agreementMapper;
+        private readonly ICsvFormatter _csvFormatter;
+        private readonly IExcelFormatter _excelFormatter;
 
         public AgreementOrchestrator(
             IMediator mediator,
             IHashingService hashingService,
             IProviderCommitmentsLogger logger,
-            IAgreementMapper agreementMapper)
+            IAgreementMapper agreementMapper,
+            ICsvFormatter csvFormatter,
+            IExcelFormatter excelFormatter)
         : base(mediator, hashingService, logger)
         {
             _agreementMapper = agreementMapper;
+            _csvFormatter = csvFormatter;
+            _excelFormatter = excelFormatter;
         }
 
         public async Task<AgreementsViewModel> GetAgreementsViewModel(long providerId)
         {
             Logger.Info($"Getting agreements for provider: {providerId}", providerId: providerId);
 
+            return new AgreementsViewModel
+            {
+                CommitmentAgreements = await GetCommitmentAgreements(providerId)
+            };
+        }
+
+        public async Task<byte[]> GetAgreementsAsCsv(long providerId)
+        {
+            var agreements = await GetCommitmentAgreements(providerId);
+            return _csvFormatter.Format(agreements);
+        }
+
+        public async Task<byte[]> GetAgreementsAsExcel(long providerId)
+        {
+            var agreements = await GetCommitmentAgreements(providerId);
+            return _excelFormatter.Format(agreements);
+        }
+
+        private async Task<IEnumerable<CommitmentAgreement>> GetCommitmentAgreements(long providerId)
+        {
             var response = await Mediator.SendAsync(new GetCommitmentAgreementsQueryRequest
             {
                 ProviderId = providerId
             });
 
-            return new AgreementsViewModel
-            {
-                CommitmentAgreements = response.CommitmentAgreements.Select(_agreementMapper.Map)
-                    .OrderBy(ca => ca.OrganisationName)
-                    .ThenBy(ca => ca.CohortID)
-            };
+            return response.CommitmentAgreements.Select(_agreementMapper.Map)
+                .OrderBy(ca => ca.OrganisationName)
+                .ThenBy(ca => ca.CohortID);
         }
     }
 }
---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/Formatters/ICsvFormatter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Agreement;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Formatters
+{
+    public interface ICsvFormatter
+    {
+        byte[] Format(IEnumerable<CommitmentAgreement> commitmentAgreements);
+    }
+}
