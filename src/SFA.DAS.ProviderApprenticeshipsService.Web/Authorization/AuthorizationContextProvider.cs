8ff41cc383b01ae2dabdbfee2e801e9af03b4ecf
Phil Davies <github@phildavies.co.uk>
2018-10-30 14:32:41 +0000

    update SFA.DAS.Authorizations (requires coming version now to run!)

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Authorization/AuthorizationContextProvider.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Authorization/AuthorizationContextProvider.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Web;
 using System.Web.Routing;
 using SFA.DAS.Authorization;
@@ -42,45 +40,6 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Authorization
         {
             var accountLegalEntityPublicHashedId = (string)routeValueDictionary[RouteDataKeys.AccountLegalEntityPublicHashedId];
             return accountLegalEntityPublicHashedId != null ? _publicHashingService.DecodeValue(accountLegalEntityPublicHashedId) : (long?)null;
-        }
-    }
-
-    public class AuthorizationContext : IAuthorizationContext, IEnumerable
-    {
-        private readonly Dictionary<string, object> _data = new Dictionary<string, object>();
-
-        public T Get<T>(string key)
-        {
-            if (_data.TryGetValue(key, out var value))
-            {
-                return (T)value;
-            }
-
-            throw new KeyNotFoundException($"The key '{key}' was not present in the authorization context");
-        }
-
-        public void Set<T>(string key, T value)
-        {
-            _data.Add(key, value);
-        }
-
-        public void Add<T>(string key, T value)
-        {
-            _data.Add(key, value);
-        }
-
-        public bool TryGet<T>(string key, out T value)
-        {
-            var exists = _data.TryGetValue(key, out var obj);
-
-            value = exists ? (T)obj : default(T);
-
-            return exists;
-        }
-
-        public IEnumerator GetEnumerator()
-        {
-            return ((IEnumerable) _data).GetEnumerator();
         }
     }
 }
