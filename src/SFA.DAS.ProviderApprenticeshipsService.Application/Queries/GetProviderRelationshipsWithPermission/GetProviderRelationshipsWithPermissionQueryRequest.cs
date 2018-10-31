dd9873ed6e7aed95f9bbb3574fc5cef3cb25bc75
chrisfoster76 <chrisfoster186@googlemail.com>
2018-10-31 13:07:50 +0000

    Adds application layer implementation

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application/Queries/GetProviderRelationshipsWithPermission/GetProviderRelationshipsWithPermissionQueryRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using SFA.DAS.ProviderRelationships.Types;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetProviderRelationshipsWithPermission
+{
+    public class GetProviderRelationshipsWithPermissionQueryRequest : IRequest<GetProviderRelationshipsWithPermissionQueryResponse>
+    {
+        public PermissionEnumDto Permission { get; set; }
+        public long ProviderId { get; set; }
+    }
+}
---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Application/Queries/GetProviderRelationshipsWithPermission/GetProviderRelationshipsWithPermissionQueryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using SFA.DAS.ProviderRelationships.Types;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetProviderRelationshipsWithPermission
+{
+    public class GetProviderRelationshipsWithPermissionQueryResponse
+    {
+        public IEnumerable<ProviderRelationshipResponse.ProviderRelationship> ProviderRelationships { get; set; }
+    }
+}
