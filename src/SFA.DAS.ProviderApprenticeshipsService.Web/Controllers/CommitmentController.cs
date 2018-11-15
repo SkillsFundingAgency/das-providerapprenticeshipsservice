c0a536d20cba7f64e806facf43041bd1e4109c8e
chrisfoster76 <chrisfoster186@googlemail.com>
2018-11-15 13:51:50 +0000

    Removes a stub controller action brought in from a merge

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Controllers/CommitmentController.cs
@@ -377,13 +377,6 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Controllers
             return View(viewModel);
         }
 
-        [HttpGet]
-        [Route("cohorts/create")]
-        public ActionResult Create(long providerId)
-        {
-            throw new NotImplementedException();
-        }
-
         private string GetReturnToListUrl(long providerId)
         {
             switch (GetRequestStatusFromCookie())
