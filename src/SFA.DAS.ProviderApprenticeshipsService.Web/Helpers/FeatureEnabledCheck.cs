df8015993b2aab0e106cfd4bda0b0152b557b521
Peter Williams <peter.david.williams.1991@gmail.com>
2019-01-29 14:50:22 +0000

    added helpers folder

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Helpers/FeatureEnabledCheck.cs
@@ -0,0 +1,17 @@
+﻿using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Helpers
+{
+    public static class FeatureEnabledCheck
+    {
+        public static bool IsManageReservationsEnabled(this HtmlHelper htmlHelper, long providerId)
+        {
+            var service = DependencyResolver.Current
+                .GetService<IFeatureToggleService>();
+            var blah = service.Get<Domain.Models.FeatureToggles.ManageReservations>().FeatureEnabled;
+            return blah;
+        }
+    }
+}
