855f8757e71e8cfd5136532a0c07e6ca38fc6fab
Ian Gledhill <ian@gledhills.mobi>
2019-02-08 11:26:36 +0000

    Update nuget restore for url helper

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -3,7 +3,7 @@ open Fake
 Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
-                Project = ".\\SFA.DAS.UrlHelper" })
+                Project = ".\\SFA.DAS.ProviderUrlHelper" })
     DotNetCli.Restore(fun p ->
         { p with
                 Project = ".\\SFA.DAS.ProviderUrlHelperTests" })
