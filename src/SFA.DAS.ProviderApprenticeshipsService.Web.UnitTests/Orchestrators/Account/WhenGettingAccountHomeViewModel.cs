c377671daa72696b18cc820267b179f129963bb6
chrisfoster76 <chrisfoster186@googlemail.com>
2019-03-21 14:24:34 +0000

    Adds test file

--- src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Account/WhenGettingAccountHomeViewModel.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Orchestrators/Account/WhenGettingAccountHomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using FeatureToggle;
 using FluentAssertions;
 using MediatR;
 using Moq;
@@ -10,6 +11,7 @@ using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetProvider;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetProviderHasRelationshipWithPermission;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Models.ApprenticeshipProvider;
+using SFA.DAS.ProviderApprenticeshipsService.Domain.Models.FeatureToggles;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators;
 using SFA.DAS.ProviderRelationships.Types.Models;
 
@@ -21,10 +23,17 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Acc
         private AccountOrchestrator _orchestrator;
         private Mock<IMediator> _mediator;
         private Mock<ICurrentDateTime> _currentDateTime;
+        private Mock<IFeatureToggleService> _featureToggleService;
+        private Mock<IFeatureToggle> _recruitFeatureToggle;
 
         [SetUp]
         public void Arrange()
         {
+            _recruitFeatureToggle = new Mock<IFeatureToggle>();
+            _recruitFeatureToggle.Setup(x => x.FeatureEnabled).Returns(true);
+            _featureToggleService = new Mock<IFeatureToggleService>();
+            _featureToggleService.Setup(x => x.Get<Recruit>()).Returns(() => _recruitFeatureToggle.Object);
+
             _mediator = new Mock<IMediator>();
             _mediator
                 .Setup(x => x.Send(It.IsAny<GetProviderQueryRequest>(), It.IsAny<CancellationToken>()))
@@ -47,7 +56,8 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Acc
 
             _orchestrator = new AccountOrchestrator(
                 _mediator.Object,
-                Mock.Of<ILog>()
+                Mock.Of<ILog>(),
+                _featureToggleService.Object
             );
         }
 
@@ -59,6 +69,15 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Orchestrators.Acc
             var model = await _orchestrator.GetAccountHomeViewModel(1);
 
             model.ShowAcademicYearBanner.Should().Be(expectShowBanner);
+        }
+
+        [TestCase(true)]
+        [TestCase(false)]
+        public async Task ThenDisplayOfRecruitmentLinkIsSubjectToFeatureToggle(bool toggled)
+        {
+            _recruitFeatureToggle.Setup(x => x.FeatureEnabled).Returns(toggled);
+            var model = await _orchestrator.GetAccountHomeViewModel(1);
+            Assert.AreEqual(toggled, model.ShowRecruitLink);
         }
     }
 }
