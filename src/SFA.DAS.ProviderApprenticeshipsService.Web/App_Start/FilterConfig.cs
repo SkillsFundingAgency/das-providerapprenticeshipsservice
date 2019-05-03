6a579f37f14fc842aa5c2d34f6017796d2bdfe16
chrisfoster76 <chrisfoster186@googlemail.com>
2019-05-03 11:50:43 +0100

    Adds configureawait(false) on all async calls in stub Removes dirty workaround whereby filter was not applied if stub active

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/FilterConfig.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/App_Start/FilterConfig.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
-using Microsoft.Azure;
 using SFA.DAS.Authorization.Mvc;
 using SFA.DAS.NLog.Logger.Web;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Attributes;
@@ -13,30 +11,12 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            // Not required - filters.Add(new HandleErrorAttribute()); 
             filters.Add(new InvalidStateExceptionFilter());
             filters.Add(new ProviderUkPrnCheckActionFilter());
             filters.Add(new DasRoleCheckActionFilter());
-
             filters.Add(new RequestIdActionFilter());
             filters.Add(new SessionIdActionFilter(HttpContext.Current));
-
-            if (!IsUsingStubProviderRelationshipsSetting())
-            {
-                filters.AddAuthorizationFilter();
-            }            
-        }
-
-        private static bool IsUsingStubProviderRelationshipsSetting()
-        {
-            var value = CloudConfigurationManager.GetSetting("UseStubProviderRelationships");
-
-            if (value == null)
-            {
-                return false;
-            }
-
-            return bool.TryParse(value, out var result) && result;
+            filters.AddAuthorizationFilter();
         }
     }
 }
