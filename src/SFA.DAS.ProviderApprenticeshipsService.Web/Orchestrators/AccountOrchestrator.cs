de08444b5d3cc842c1631b0fbbd50178a6b2a015
Paul Graham <paul.graham@coprime.co.uk>
2019-06-24 15:33:37 +0100

    removed featureToggle recruit link

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AccountOrchestrator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AccountOrchestrator.cs
@@ -13,7 +13,6 @@ using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetProviderHasR
 using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetUser;
 using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetUserNotificationSettings;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
-using SFA.DAS.ProviderApprenticeshipsService.Domain.Models.FeatureToggles;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Settings;
 using SFA.DAS.ProviderRelationships.Types.Models;
@@ -24,14 +23,12 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators
     {
         private readonly IMediator _mediator;
         private readonly ILog _logger;
-        private readonly IFeatureToggleService _featureToggleService;
 
         public AccountOrchestrator(IMediator mediator,
-            ILog logger, IFeatureToggleService featureToggleService)
+            ILog logger)
         {
             _mediator = mediator;
             _logger = logger;
-            _featureToggleService = featureToggleService;
         }
 
         public async Task<AccountHomeViewModel> GetAccountHomeViewModel(int providerId)
@@ -50,8 +47,7 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators
                     ProviderName = providerResponse.ProvidersView.Provider.ProviderName,
                     ProviderId = providerId,
                     ShowAcademicYearBanner = false,
-                    ShowCreateCohortLink = showCreateCohortLink,
-                    ShowRecruitLink = _featureToggleService.Get<Recruit>().FeatureEnabled
+                    ShowCreateCohortLink = showCreateCohortLink
                 };
             }
             catch (EntityNotFoundException)
