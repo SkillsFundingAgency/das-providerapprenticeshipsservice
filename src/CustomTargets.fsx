d37025fdc3b3deb8fd7879613333fcabae9c91f1
Paul Graham <paul.graham@coprime.co.uk>
2019-09-04 14:19:08 +0100

    updated customTarget.fsx so it restores the nugets for the new client API projects

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -7,4 +7,10 @@ Target "Dotnet Restore" (fun _ ->
     DotNetCli.Restore(fun p ->
         { p with
                 Project = ".\\SFA.DAS.ProviderUrlHelperTests" })
+    DotNetCli.Restore(fun p ->
+        { p with
+                Project = ".\\SFA.DAS.PAS.Account.Api.ClientV2" })
+    DotNetCli.Restore(fun p ->
+        { p with
+                Project = ".\\SFA.DAS.PAS.Account.Api.Client.UnitTests" })
 )
