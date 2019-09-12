2bbe2435dd1ef43805bdcabf64e5ce49d3a17427
Paul Graham <paul.graham@coprime.co.uk>
2019-09-12 09:17:24 +0100

    renamed file to match class name

--- src/CustomTargets.fsx
+++ src/CustomTargets.fsx
@@ -12,5 +12,5 @@ Target "Dotnet Restore" (fun _ ->
                 Project = ".\\SFA.DAS.PAS.Account.Api.ClientV2" })
     DotNetCli.Restore(fun p ->
         { p with
-                Project = ".\\SFA.DAS.PAS.Account.Api.Client.UnitTests" })
+                Project = ".\\SFA.DAS.PAS.Account.Api.ClientV2.UnitTests" })
 )
