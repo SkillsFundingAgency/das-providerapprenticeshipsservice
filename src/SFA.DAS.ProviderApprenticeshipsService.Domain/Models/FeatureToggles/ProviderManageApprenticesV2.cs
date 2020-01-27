4129ae726781d359335a8c1b73225c53a899d482
Peter Williams <peter.david.williams.1991@gmail.com>
2020-01-27 13:56:55 +0000

    Added toggle for ManageApprentices V2 page and set it to off on local

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Domain/Models/FeatureToggles/ProviderManageApprenticesV2.cs
@@ -0,0 +1,8 @@
+﻿using FeatureToggle;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Domain.Models.FeatureToggles
+{
+    public class ProviderManageApprenticesV2 : SimpleFeatureToggle
+    {
+    }
+}
