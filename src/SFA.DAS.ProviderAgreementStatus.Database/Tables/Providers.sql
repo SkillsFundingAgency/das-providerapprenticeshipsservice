f83a55c23f6228c6df308241a9a95bdcfc955564
Paul Graham <paul.graham@coprime.co.uk>
2020-05-13 14:57:50 +0100

    added new columns

--- src/SFA.DAS.ProviderAgreementStatus.Database/Tables/Providers.sql
+++ src/SFA.DAS.ProviderAgreementStatus.Database/Tables/Providers.sql
@@ -4,5 +4,10 @@
     [Name] NVARCHAR(100) NOT NULL,
     [Created] DATETIME2 NOT NULL,
     [Updated] DATETIME2 NULL,
+    [UpdatedFromIDAMS] DATETIMEOFFSET NULL, 
     CONSTRAINT [PK_Providers] PRIMARY KEY CLUSTERED ([Ukprn] ASC)
 )
+GO 
+
+CREATE INDEX [IX_Provider_UpdateFromIDAMS] ON [dbo].[Providers] ([UpdatedFromIDAMS])
+GO
