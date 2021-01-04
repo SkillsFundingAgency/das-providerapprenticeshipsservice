7048fc6f9d50905eae1ee1df0b63776c6ffcb20a
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2021-01-04 20:01:15 +0000

    Correct web job configuration for provider import

--- src/SFA.DAS.PAS.ImportProvider.WebJob/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.PAS.ImportProvider.WebJob/DependencyResolution/DefaultRegistry.cs
@@ -5,7 +5,9 @@ using StructureMap;
 using System;
 using System.Configuration;
 using System.Net.Http;
+using SFA.DAS.AutoConfiguration;
 using SFA.DAS.Commitments.Api.Client;
+using SFA.DAS.Commitments.Api.Client.Configuration;
 using SFA.DAS.Commitments.Api.Client.Interfaces;
 using SFA.DAS.Http;
 using SFA.DAS.Http.Configuration;
@@ -27,9 +29,9 @@ namespace SFA.DAS.PAS.ImportProvider.WebJob.DependencyResolution
                    scan.RegisterConcreteTypesAgainstTheFirstInterface();
                });
 
-            var config = GetConfiguration("SFA.DAS.ProviderApprenticeshipsService");
-            For<ProviderApprenticeshipsServiceConfiguration>().Use(config);
-            For<IConfiguration>().Use<ProviderApprenticeshipsServiceConfiguration>();
+            For<CommitmentsApiClientConfiguration>().Use(c => c.GetInstance<IAutoConfigurationService>().Get<CommitmentsApiClientConfiguration>("SFA.DAS.CommitmentsAPI")).Singleton();
+            For<ICommitmentsApiClientConfiguration>().Use(c => c.GetInstance<CommitmentsApiClientConfiguration>());
+            
             For<IProviderCommitmentsApi>().Use<ProviderCommitmentsApi>()
                 .Ctor<HttpClient>().Is(c => GetHttpClient(c));
             
@@ -63,9 +65,9 @@ namespace SFA.DAS.PAS.ImportProvider.WebJob.DependencyResolution
         
         private HttpClient GetHttpClient(IContext context)
         {
-            var config = context.GetInstance<ProviderApprenticeshipsServiceConfiguration>();
+            var config = context.GetInstance<CommitmentsApiClientConfiguration>();
 
-            var httpClientBuilder = string.IsNullOrWhiteSpace(config.CommitmentsApi.ClientId)
+            var httpClientBuilder = string.IsNullOrWhiteSpace(config.ClientId)
                 ? new HttpClientBuilder().WithBearerAuthorisationHeader(new JwtBearerTokenGenerator(config as IJwtClientConfiguration))
                 : new HttpClientBuilder().WithBearerAuthorisationHeader(new AzureActiveDirectoryBearerTokenGenerator(config as IAzureActiveDirectoryClientConfiguration));
 
