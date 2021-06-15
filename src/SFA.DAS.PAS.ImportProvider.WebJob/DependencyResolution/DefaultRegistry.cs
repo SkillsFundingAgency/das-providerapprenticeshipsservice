d9c8b5e3de9c03b5663f9ddf849726d25ccea409
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-06-15 14:29:01 +0100

    Cleanup no longer used configuation getting code

--- src/SFA.DAS.PAS.ImportProvider.WebJob/DependencyResolution/DefaultRegistry.cs
+++ src/SFA.DAS.PAS.ImportProvider.WebJob/DependencyResolution/DefaultRegistry.cs
@@ -1,9 +1,6 @@
-﻿using SFA.DAS.Configuration;
-using SFA.DAS.Configuration.AzureTableStorage;
-using SFA.DAS.NLog.Logger;
+﻿using SFA.DAS.NLog.Logger;
 using StructureMap;
 using System;
-using System.Configuration;
 using System.Net.Http;
 using SFA.DAS.AutoConfiguration;
 using SFA.DAS.Commitments.Api.Client;
@@ -14,7 +11,6 @@ using SFA.DAS.Http.Configuration;
 using SFA.DAS.Http.TokenGenerators;
 using SFA.DAS.NLog.Logger.Web.MessageHandlers;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Configuration;
-using IConfiguration = SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.IConfiguration;
 
 namespace SFA.DAS.PAS.ImportProvider.WebJob.DependencyResolution
 {
@@ -39,28 +35,6 @@ namespace SFA.DAS.PAS.ImportProvider.WebJob.DependencyResolution
                x.ParentType,
                new DummyRequestContext(),
                null)).AlwaysUnique();
-        }
-
-        private ProviderApprenticeshipsServiceConfiguration GetConfiguration(string serviceName)
-        {
-            var environment = Environment.GetEnvironmentVariable("DASENV");
-            if (string.IsNullOrEmpty(environment))
-            {
-                environment = ConfigurationManager.AppSettings["EnvironmentName"];
-            }
-
-            var configurationRepository = GetConfigurationRepository();
-            var configurationService = new ConfigurationService(configurationRepository,
-                new ConfigurationOptions(serviceName, environment, "1.0"));
-
-            var result = configurationService.Get<ProviderApprenticeshipsServiceConfiguration>();
-
-            return result;
-        }
-
-        private static IConfigurationRepository GetConfigurationRepository()
-        {
-            return new AzureTableStorageConfigurationRepository(ConfigurationManager.AppSettings["ConfigurationStorageConnectionString"]);
         }
         
         private HttpClient GetHttpClient(IContext context)
