9af0efed41b2d8be26ed1fb4e55a7a685e9a8241
Paul Graham <paul.graham@coprime.co.uk>
2021-07-19 16:53:27 +0100

    check email is required for provider

--- src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Services/CommitmentsV2ApiClient.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Services/CommitmentsV2ApiClient.cs
@@ -1,4 +1,6 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using SFA.DAS.Authentication.Extensions.Legacy;
@@ -10,10 +12,12 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Services
 {
     public class CommitmentsV2ApiClient : ApiClientBase, ICommitmentsV2ApiClient
     {
+        private readonly HttpClient _httpClient;
         private readonly CommitmentsApiClientV2Configuration _config;
 
         public CommitmentsV2ApiClient(HttpClient httpClient, CommitmentsApiClientV2Configuration config) : base(httpClient)
         {
+            _httpClient = httpClient;
             _config = config;
         }
 
@@ -22,6 +26,21 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Services
             var url = $"{BaseUrl()}api/cohorts/{cohortId}";
             var response = JsonConvert.DeserializeObject<GetCohortResponse>(await GetAsync(url));
             return response;
+        }
+
+        public async Task<bool> ApprenticeEmailRequired(long providerId)
+        {
+            var url = $"{BaseUrl()}api/authorization/features/providers/{providerId}/apprentice-email-required";
+            var response = await _httpClient.GetAsync(url);
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    return true;
+                case HttpStatusCode.NotFound:
+                    return false;
+                default:
+                    throw new ApplicationException("Unexpected status code returned");
+            }
         }
 
         private string BaseUrl()
