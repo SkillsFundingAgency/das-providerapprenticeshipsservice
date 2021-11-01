78570f4353edf3a723947aee33dff9a0eadc4c6f
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-11-01 15:03:19 +0000

    [CON-4177] Apply enhanced select and allow searching of already loaded data in the viewmodel rather than making a request for evert letter after char 3

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/dist/javascripts/autocomplete.js
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/dist/javascripts/autocomplete.js
@@ -1,44 +1,28 @@
-var autocompleteInputs = document.querySelectorAll(".app-autocomplete");
+var idSelectField = 'search-agreement';
 
-  if (autocompleteInputs.length > 0) {
-
-    for (var i = 0; i < autocompleteInputs.length; i++) {
-
-      var input = autocompleteInputs[i]
-      var container = document.createElement('div');
-      var apiUrl = input.dataset.autocompleteUrl;
-
-      container.className = "das-autocomplete-wrap"
-      input.parentNode.replaceChild(container, input);
-
-      var getSuggestions = function (query, updateResults) {
-        var results = [];
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-          if (xhr.readyState === 4) {
-            var jsonResponse = JSON.parse(xhr.responseText);
-            results = jsonResponse.map(function (r) {                
-                return r.OrganisationName;
-            });
-            updateResults(results);
-          }
-        }
-        xhr.open("GET", apiUrl + '?searchTerm=' + query, true);
-        xhr.send();
-      };
-
-      accessibleAutocomplete({
-        element: container,
-        id: input.id,
-        name: input.name,
-        defaultValue: input.value,
-        displayMenu: 'overlay',
-        showNoOptionsFound: false,
+var selectEl = document.querySelector('#' + idSelectField);
+if (selectEl) {
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: selectEl,
         minLength: 3,
-        source: getSuggestions,
-        placeholder: "",
-        confirmOnBlur: false,
-        autoselect: true
-      });
-    }
-  }
+        autoselect: true,
+        defaultValue: selectEl.dataset.autocompleteDefault,
+        displayMenu: 'overlay',
+        placeholder: '',
+        onConfirm: function (opt) {
+            var txtInput = document.querySelector('#' + idSelectField);
+            var searchString = opt || txtInput.value;
+            var requestedOption = [].filter.call(this.selectElement.options,
+                function (option) {
+                    return (option.textContent || option.innerText) === searchString
+                }
+            )[0];
+            if (requestedOption) {
+                requestedOption.selected = true;
+            } else {
+                this.selectElement.selectedIndex = 0;
+            }
+        }
+
+    });
+}
