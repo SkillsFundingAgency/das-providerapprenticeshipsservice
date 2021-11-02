74f2ee6b2c4ab76b0a5c6176337bddc5b688520d
Corey <corey.faulconbridge@digital.education.gov.uk>
2021-11-02 09:27:20 +0000

    [CON-4177] Update to populate autocomplete with all results after filtering

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/Agreement/AgreementsViewModel.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/Agreement/AgreementsViewModel.cs
@@ -4,7 +4,8 @@ namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models.Agreement
 {
     public class AgreementsViewModel
     {
-        public IEnumerable<CommitmentAgreement> CommitmentAgreements;
+        public IEnumerable<CommitmentAgreement> CommitmentAgreements { get; set; }
         public string SearchText { get; set; }
+        public IEnumerable<string> AllProviderOrganisationNames { get; set; }
     }
 }
