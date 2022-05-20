ced7cc2aebc1e423a4d009e36369e09bb6df208f
Chris Woodcock <chris@humesoftware.com>
2022-05-20 11:04:00 +0100

    Add config and test console to api client v1

---
+++ src/SFA.DAS.PAS.Account.Api.Client/PasAccountApiConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace SFA.DAS.PAS.Account.Api.Client
+{
+    public class PasAccountApiConfiguration : IPasAccountApiConfiguration
+    {
+        public string ApiBaseUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string IdentifierUri { get; set; }
+        public string Tenant { get; set; }
+    }
+}
