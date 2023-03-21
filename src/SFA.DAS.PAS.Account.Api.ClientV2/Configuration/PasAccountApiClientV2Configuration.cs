c56fe0c7786d1c7b38fb97a0587015345f2dba59
David Rimar <david.rimar@digital.education.gov.uk>
2023-03-21 11:15:43 +0000

    remove client factory and service collection extensions from ApiClientV2 project

--- src/SFA.DAS.PAS.Account.Api.ClientV2/Configuration/PasAccountApiClientV2Configuration.cs
+++ src/SFA.DAS.PAS.Account.Api.ClientV2/Configuration/PasAccountApiClientV2Configuration.cs
@@ -2,12 +2,9 @@
 
 namespace SFA.DAS.PAS.Account.Api.ClientV2.Configuration
 {
-    public class PasAccountApiClientV2Configuration : IAzureActiveDirectoryClientConfiguration, IManagedIdentityClientConfiguration
+    public class PasAccountApiClientV2Configuration : IManagedIdentityClientConfiguration
     {
         public string ApiBaseUrl { get; set; }
-        public string Tenant { get; set; }
-        public string ClientId { get; set; }
-        public string ClientSecret { get; set; }
         public string IdentifierUri { get; set; }
     }
 }
