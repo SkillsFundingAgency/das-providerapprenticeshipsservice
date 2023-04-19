403411d082d24773010dbacdde07e5853f9a706c
Jon Harris <jon.harris@digital.education.gov.uk>
2023-04-19 14:34:15 +0100

    File scoped namespaces for Application project

--- src/SFA.DAS.PAS.Account.Application/Queries/GetProviderAgreement/GetProviderAgreementQueryHandler.cs
+++ src/SFA.DAS.PAS.Account.Application/Queries/GetProviderAgreement/GetProviderAgreementQueryHandler.cs
@@ -3,29 +3,28 @@ using SFA.DAS.ProviderApprenticeshipsService.Domain.Enums;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Configuration;
 
-namespace SFA.DAS.PAS.Account.Application.Queries.GetProviderAgreement
+namespace SFA.DAS.PAS.Account.Application.Queries.GetProviderAgreement;
+
+public class GetProviderAgreementQueryHandler : IRequestHandler<GetProviderAgreementQueryRequest, GetProviderAgreementQueryResponse>
 {
-    public class GetProviderAgreementQueryHandler : IRequestHandler<GetProviderAgreementQueryRequest, GetProviderAgreementQueryResponse>
+    private readonly IProviderAgreementStatusRepository _providerAgreementStatusRepository;
+    private readonly IPasAccountApiConfiguration _configuration;
+
+    public GetProviderAgreementQueryHandler(IProviderAgreementStatusRepository providerAgreementStatusRepository, IPasAccountApiConfiguration configuration)
     {
-        private readonly IProviderAgreementStatusRepository _providerAgreementStatusRepository;
-        private readonly IPasAccountApiConfiguration _configuration;
+        _providerAgreementStatusRepository = providerAgreementStatusRepository;
+        _configuration = configuration;
+    }
 
-        public GetProviderAgreementQueryHandler(IProviderAgreementStatusRepository providerAgreementStatusRepository, IPasAccountApiConfiguration configuration)
-        {
-            _providerAgreementStatusRepository = providerAgreementStatusRepository;
-            _configuration = configuration;
-        }
+    public async Task<GetProviderAgreementQueryResponse> Handle(GetProviderAgreementQueryRequest message, CancellationToken cancellationToken)
+    {
+        if(!_configuration.CheckForContractAgreements)
+            return new GetProviderAgreementQueryResponse { HasAgreement = ProviderAgreementStatus.Agreed };
 
-        public async Task<GetProviderAgreementQueryResponse> Handle(GetProviderAgreementQueryRequest message, CancellationToken cancellationToken)
-        {
-            if(!_configuration.CheckForContractAgreements)
-                return new GetProviderAgreementQueryResponse { HasAgreement = ProviderAgreementStatus.Agreed };
-
-            var res = await _providerAgreementStatusRepository.GetContractEvents(message.ProviderId);
-            var providerAgreementStatus = res.Any(m => m.Status.Equals("approved", StringComparison.CurrentCultureIgnoreCase))
-                        ? ProviderAgreementStatus.Agreed : ProviderAgreementStatus.NotAgreed;
+        var res = await _providerAgreementStatusRepository.GetContractEvents(message.ProviderId);
+        var providerAgreementStatus = res.Any(m => m.Status.Equals("approved", StringComparison.CurrentCultureIgnoreCase))
+            ? ProviderAgreementStatus.Agreed : ProviderAgreementStatus.NotAgreed;
             
-            return new GetProviderAgreementQueryResponse { HasAgreement = providerAgreementStatus };
-        }
+        return new GetProviderAgreementQueryResponse { HasAgreement = providerAgreementStatus };
     }
 }
