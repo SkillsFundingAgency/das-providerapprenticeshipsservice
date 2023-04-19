17e5cda6bbfa061e689451e1ff5360f9bc96c13a
Jon Harris <jon.harris@digital.education.gov.uk>
2023-04-19 14:27:45 +0100

    Orchestrators tidy

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AgreementOrchestrator.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Orchestrators/AgreementOrchestrator.cs
@@ -1,69 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MediatR;
-using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitmentAgreements;
+﻿using SFA.DAS.ProviderApprenticeshipsService.Application.Queries.GetCommitmentAgreements;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
+using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.Logging;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Agreement;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators.Mappers;
 
-namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators;
+
+public interface IAgreementOrchestrator
 {
-    public interface IAgreementOrchestrator
+    Task<AgreementsViewModel> GetAgreementsViewModel(long providerId, string organisation);
+}
+
+public sealed class AgreementOrchestrator : IAgreementOrchestrator
+{
+    private readonly IAgreementMapper _agreementMapper;
+    private readonly IMediator _mediator;
+    private readonly IProviderCommitmentsLogger _logger;
+
+    public AgreementOrchestrator(
+        IMediator mediator,
+        IProviderCommitmentsLogger logger,
+        IAgreementMapper agreementMapper)
     {
-        Task<AgreementsViewModel> GetAgreementsViewModel(long providerId, string organisation);
+        _agreementMapper = agreementMapper;
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
-    public sealed class AgreementOrchestrator : IAgreementOrchestrator
+    public async Task<AgreementsViewModel> GetAgreementsViewModel(long providerId, string organisation)
     {
-        private readonly IAgreementMapper _agreementMapper;
-        private readonly IMediator _mediator;
-        private readonly IProviderCommitmentsLogger _logger;
+        _logger.Info($"Getting agreements for provider: {providerId}", providerId);
 
-        public AgreementOrchestrator(
-            IMediator mediator,
-            IProviderCommitmentsLogger logger,
-            IAgreementMapper agreementMapper)
+        var commitmentAgreements = await GetCommitmentAgreements(providerId);
+
+        var filteredCommitmentAgreements = string.IsNullOrEmpty(organisation)
+            ? commitmentAgreements
+            : commitmentAgreements.Where(v => string.IsNullOrWhiteSpace(organisation.ToLower()) || (string.IsNullOrWhiteSpace(v.OrganisationName) == false && v.OrganisationName.ToLower().Replace(" ", String.Empty).Contains(organisation.ToLower().Replace(" ", String.Empty))));
+
+        return new AgreementsViewModel
         {
-            _agreementMapper = agreementMapper;
-            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        }
+            CommitmentAgreements = filteredCommitmentAgreements.ToList(),
+            AllProviderOrganisationNames = commitmentAgreements.Select(ca => ca.OrganisationName).ToList(),
+            SearchText = organisation
+        };
+    }
 
-        public async Task<AgreementsViewModel> GetAgreementsViewModel(long providerId, string organisation)
+    private async Task<IEnumerable<CommitmentAgreement>> GetCommitmentAgreements(long providerId)
+    {
+        var response = await _mediator.Send(new GetCommitmentAgreementsQueryRequest
         {
-            _logger.Info($"Getting agreements for provider: {providerId}", providerId);
+            ProviderId = providerId
+        });
 
-            var commitmentAgreements = await GetCommitmentAgreements(providerId);
+        var result = response.CommitmentAgreements.Select(_agreementMapper.Map);
 
-            var filteredCommitmentAgreements = string.IsNullOrEmpty(organisation)
-                ? commitmentAgreements
-                : commitmentAgreements.Where(v => string.IsNullOrWhiteSpace(organisation.ToLower()) || (string.IsNullOrWhiteSpace(v.OrganisationName) == false && v.OrganisationName.ToLower().Replace(" ", String.Empty).Contains(organisation.ToLower().Replace(" ", String.Empty))));
-
-            return new AgreementsViewModel
-            {
-                CommitmentAgreements = filteredCommitmentAgreements.ToList(),
-                AllProviderOrganisationNames = commitmentAgreements.Select(ca => ca.OrganisationName).ToList(),
-                SearchText = organisation
-            };
-        }
-
-        private async Task<IEnumerable<CommitmentAgreement>> GetCommitmentAgreements(long providerId)
-        {
-            var response = await _mediator.Send(new GetCommitmentAgreementsQueryRequest
-            {
-                ProviderId = providerId
-            });
-
-            var result = response.CommitmentAgreements.Select(_agreementMapper.Map);
-
-            return result
-                    .OrderBy(v => v.OrganisationName)
-                    .ThenBy(ca => ca.AgreementID)
-                    .GroupBy(m => new { m.OrganisationName, m.AgreementID })
-                    .Select(x => x.FirstOrDefault())
-                    .ToList();
-        }
+        return result
+            .OrderBy(v => v.OrganisationName)
+            .ThenBy(ca => ca.AgreementID)
+            .GroupBy(m => new { m.OrganisationName, m.AgreementID })
+            .Select(x => x.FirstOrDefault())
+            .ToList();
     }
 }
