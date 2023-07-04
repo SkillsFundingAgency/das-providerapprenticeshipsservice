2acb7fdd8f870796ee225b43e984a7f380bc6715
Jon Harris <jon.harris@digital.education.gov.uk>
2023-07-04 14:48:59 +0100

    Added missing ChainedTokenCredential wireup

--- src/SFA.DAS.PAS.ContractAgreements.WebJob/Extensions/HostBuilderExtensions.cs
+++ src/SFA.DAS.PAS.ContractAgreements.WebJob/Extensions/HostBuilderExtensions.cs
@@ -1,86 +1,94 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using System.Reflection;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Client;
 using SFA.DAS.Configuration;
 using SFA.DAS.Configuration.AzureTableStorage;
 using SFA.DAS.PAS.ContractAgreements.WebJob.Configuration;
 using SFA.DAS.PAS.ContractAgreements.WebJob.ContractFeed;
 using SFA.DAS.PAS.ContractAgreements.WebJob.Interfaces;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
-using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Configuration;
+using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.Data;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Data;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Services;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Models;
-using System;
-using System.Reflection;
-using Azure.Identity;
-using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.Data;
-using Microsoft.Identity.Client;
 
-namespace SFA.DAS.PAS.ContractAgreements.WebJob.Extensions
+namespace SFA.DAS.PAS.ContractAgreements.WebJob.Extensions;
+
+public static class HostBuilderExtensions
 {
-    public static class HostBuilderExtensions
+    public static IHostBuilder ConfigureServices(this IHostBuilder hostBuilder)
     {
-        public static IHostBuilder ConfigureServices(this IHostBuilder hostBuilder)
+        hostBuilder.ConfigureServices((context, services) =>
         {
-            hostBuilder.ConfigureServices((context, services) =>
-            {
-                services.Configure<ContractFeedConfiguration>(context.Configuration.GetSection(ConfigurationKeys.ContractAgreements));
-                services.AddSingleton<IContractFeedConfiguration>(isp => isp.GetService<IOptions<ContractFeedConfiguration>>().Value);
-                services.AddSingleton<IBaseConfiguration>(isp => isp.GetService<IOptions<ContractFeedConfiguration>>().Value);
-                services.AddSingleton<IConfidentialClientApplication, ConfidentialClientApplication>();
-                
-                services.AddSingleton(new ChainedTokenCredential(
-                    new ManagedIdentityCredential(),
-                    new AzureCliCredential())
-                );
+            services.Configure<ContractFeedConfiguration>(
+                context.Configuration.GetSection(ConfigurationKeys.ContractAgreements));
+            services.AddSingleton<IContractFeedConfiguration>(isp =>
+                isp.GetService<IOptions<ContractFeedConfiguration>>().Value);
+            services.AddSingleton<IBaseConfiguration>(
+                isp => isp.GetService<IOptions<ContractFeedConfiguration>>().Value);
+            services.AddSingleton<IConfidentialClientApplication, ConfidentialClientApplication>();
 
-                services.AddTransient<ICurrentDateTime, CurrentDateTime>();
-                services.AddTransient<IContractFeedProcessorHttpClient, ContractFeedProcessorHttpClient>();
-                services.AddTransient<IContractFeedEventValidator, ContractFeedEventValidator>();
-                services.AddTransient<IContractFeedReader, ContractFeedReader>();
-                services.AddTransient<IContractDataProvider, ContractFeedProcessor>();
-                services.AddTransient<IProviderAgreementStatusRepository, ProviderAgreementStatusRepository>();
-                services.AddTransient<IProviderAgreementStatusService, ProviderAgreementStatusService>();
-                services.AddLogging();
-            });
+            services.AddSingleton(new ChainedTokenCredential(
+                new ManagedIdentityCredential(),
+                new AzureCliCredential())
+            );
 
-            return hostBuilder;
-        }
+            services.AddTransient<ICurrentDateTime, CurrentDateTime>();
+            services.AddTransient<IContractFeedProcessorHttpClient, ContractFeedProcessorHttpClient>();
+            services.AddTransient<IContractFeedEventValidator, ContractFeedEventValidator>();
+            services.AddTransient<IContractFeedReader, ContractFeedReader>();
+            services.AddTransient<IContractDataProvider, ContractFeedProcessor>();
+            services.AddTransient<IProviderAgreementStatusRepository, ProviderAgreementStatusRepository>();
+            services.AddTransient<IProviderAgreementStatusService, ProviderAgreementStatusService>();
+            services.AddLogging();
+        });
 
-        public static IHostBuilder AddConfiguration(this IHostBuilder hostBuilder)
+        return hostBuilder;
+    }
+
+    public static IHostBuilder AddConfiguration(this IHostBuilder hostBuilder)
+    {
+        return hostBuilder.ConfigureAppConfiguration((context, builder) =>
         {
-            return hostBuilder.ConfigureAppConfiguration((context, builder) =>
-            {
-                var environment = context.HostingEnvironment.EnvironmentName;
+            var environment = context.HostingEnvironment.EnvironmentName;
 
-                if (environment.Equals("LOCAL") || environment.Equals("AT") || environment.Equals("TEST"))
-                {
-                    PopulateSystemDetails(environment);
-                }
-
-                builder.AddJsonFile("appsettings.json", true, true)
-                       .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
-                       .AddAzureTableStorage(ConfigurationKeys.ContractAgreements)
-                       .AddEnvironmentVariables();
-            });
-        }
-        public static IHostBuilder UseDasEnvironment(this IHostBuilder hostBuilder)
-        {
-            var environment = Environment.GetEnvironmentVariable("DASENV");
-            if (string.IsNullOrEmpty(environment))
+            if (environment.Equals("LOCAL") || environment.Equals("AT") || environment.Equals("TEST"))
             {
-                environment = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName);
+                PopulateSystemDetails(environment);
             }
 
-            return hostBuilder.UseEnvironment(environment);
+            builder.AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", true, true)
+                .AddAzureTableStorage(ConfigurationKeys.ContractAgreements)
+                .AddEnvironmentVariables();
+        });
+    }
+
+    public static IHostBuilder UseDasEnvironment(this IHostBuilder hostBuilder)
+    {
+        var environment = Environment.GetEnvironmentVariable("DASENV");
+        if (string.IsNullOrEmpty(environment))
+        {
+            environment = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName);
         }
 
-        private static void PopulateSystemDetails(string envName)
+        return hostBuilder.UseEnvironment(environment);
+    }
+
+    private static void PopulateSystemDetails(string envName)
+    {
+        SystemDetails.EnvironmentName = envName;
+        
+        var version = Assembly.GetExecutingAssembly().GetName().Version;
+        
+        if (version != null)
         {
-            SystemDetails.EnvironmentName = envName;
-            SystemDetails.VersionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            SystemDetails.VersionNumber = version.ToString();
         }
     }
 }
--- src/SFA.DAS.PAS.ImportProvider.WebJob/Extensions/HostBuilderExtensions.cs
+++ src/SFA.DAS.PAS.ImportProvider.WebJob/Extensions/HostBuilderExtensions.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SFA.DAS.Http.TokenGenerators;
-using SFA.DAS.Http;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using SFA.DAS.PAS.ImportProvider.WebJob.Configuration;
 using SFA.DAS.PAS.ImportProvider.WebJob.Services;
 using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces;
+using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.Data;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Configuration;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Data;
-using System;
-using System.Net.Http;
-using Azure.Identity;
-using SFA.DAS.Configuration.AzureTableStorage;
-using SFA.DAS.Configuration;
-using SFA.DAS.PAS.ImportProvider.WebJob.Configuration;
-using Microsoft.Extensions.Options;
-using SFA.DAS.ProviderApprenticeshipsService.Domain.Interfaces.Data;
 using SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Services;
 
 namespace SFA.DAS.PAS.ImportProvider.WebJob.Extensions;
@@ -25,18 +22,21 @@ public static class HostBuilderExtensions
     {
         hostBuilder.ConfigureServices((context, services) =>
         {
-            services.Configure<ProviderApprenticeshipsServiceConfiguration>(context.Configuration.GetSection(ConfigurationKeys.ProviderApprenticeshipsService));
-            services.AddSingleton<IBaseConfiguration>(isp => isp.GetService<IOptions<ProviderApprenticeshipsServiceConfiguration>>().Value);
+            services.Configure<ProviderApprenticeshipsServiceConfiguration>(
+                context.Configuration.GetSection(ConfigurationKeys.ProviderApprenticeshipsService));
+            services.AddSingleton<IBaseConfiguration>(isp =>
+                isp.GetService<IOptions<ProviderApprenticeshipsServiceConfiguration>>().Value);
 
-            services.Configure<CommitmentsApiClientV2Configuration>(c => context.Configuration.GetSection(ConfigurationKeys.CommitmentsApiClientV2).Bind(c));
+            services.Configure<CommitmentsApiClientV2Configuration>(c =>
+                context.Configuration.GetSection(ConfigurationKeys.CommitmentsApiClientV2).Bind(c));
             services.AddSingleton(cfg => cfg.GetService<IOptions<CommitmentsApiClientV2Configuration>>().Value);
             services.AddHttpClient<ICommitmentsV2ApiClient, CommitmentsV2ApiClient>();
-                
+
             services.AddSingleton(new ChainedTokenCredential(
                 new ManagedIdentityCredential(),
                 new AzureCliCredential())
             );
-                
+
             services.AddTransient<IProviderRepository, ProviderRepository>();
             services.AddTransient<IImportProviderService, ImportProviderService>();
             services.AddLogging();
@@ -62,19 +62,8 @@ public static class HostBuilderExtensions
     {
         var environment = Environment.GetEnvironmentVariable("DASENV");
         if (string.IsNullOrEmpty(environment))
-        {
             environment = Environment.GetEnvironmentVariable(EnvironmentVariableNames.EnvironmentName);
-        }
 
         return hostBuilder.UseEnvironment(environment);
-    }
-
-    private static HttpClient GetHttpV2Client(CommitmentsApiClientV2Configuration commitmentsV2Config, IConfiguration config)
-    {
-        var httpClientBuilder = config.IsLocal()
-            ? new HttpClientBuilder()
-            : new HttpClientBuilder().WithBearerAuthorisationHeader(new ManagedIdentityTokenGenerator(commitmentsV2Config));
-            
-        return httpClientBuilder.Build();
     }
 }
