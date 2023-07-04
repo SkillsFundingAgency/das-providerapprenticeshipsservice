a60b1f856f9df96b5b4ccfe376c57e0f619b4181
Jon Harris <jon.harris@digital.education.gov.uk>
2023-07-04 10:00:55 +0100

    Rename class

--- src/SFA.DAS.PAS.Account.Api/ServiceRegistrations/ValidationServiceRegistration.cs
+++ src/SFA.DAS.PAS.Account.Api/ServiceRegistrations/ValidationServiceRegistration.cs
@@ -3,7 +3,7 @@ using SFA.DAS.PAS.Account.Application.Commands.SendNotification;
 
 namespace SFA.DAS.PAS.Account.Api.ServiceRegistrations;
 
-public static class FluentValidationServiceRegistration
+public static class ValidationServiceRegistration
 {
     public static IServiceCollection AddApiValidators(this IServiceCollection services)
     {
