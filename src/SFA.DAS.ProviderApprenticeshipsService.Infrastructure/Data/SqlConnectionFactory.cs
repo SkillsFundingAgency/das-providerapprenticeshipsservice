b0f8df7186fc865af3cc2e235e65953780bea845
Jon Harris <jon.harris@digital.education.gov.uk>
2023-07-12 14:04:58 +0100

    Using AzureServiceTokenProvider instead of ChainedTokenCredential for SQL MI

--- src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Data/SqlConnectionFactory.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Infrastructure/Data/SqlConnectionFactory.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Azure.Core;
 using Azure.Identity;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Data.SqlClient;
 
 namespace SFA.DAS.ProviderApprenticeshipsService.Infrastructure.Data;
@@ -9,7 +11,7 @@ public static class SqlConnectionFactory
 {
     private const string AzureResource = "https://database.windows.net/";
 
-    public static SqlConnection GetConnection(string connectionString)
+    public static async Task<SqlConnection> GetConnectionAsync(string connectionString)
     {
         if (string.IsNullOrEmpty(connectionString))
         {
@@ -18,22 +20,18 @@ public static class SqlConnectionFactory
 
         var connectionStringBuilder = new SqlConnectionStringBuilder(connectionString);
         var useManagedIdentity = !connectionStringBuilder.IntegratedSecurity && string.IsNullOrEmpty(connectionStringBuilder.UserID);
-        
+
         if (!useManagedIdentity)
         {
             return new SqlConnection(connectionString);
         }
 
-        var azureServiceTokenProvider = new ChainedTokenCredential(
-            new ManagedIdentityCredential(),
-            new AzureCliCredential(),
-            new VisualStudioCodeCredential(),
-            new VisualStudioCredential());
+        var azureServiceTokenProvider = new AzureServiceTokenProvider();
 
         return new SqlConnection
         {
             ConnectionString = connectionString,
-            AccessToken = azureServiceTokenProvider.GetToken(new TokenRequestContext(scopes: new[] { AzureResource })).Token
+            AccessToken = await azureServiceTokenProvider.GetAccessTokenAsync(AzureResource)
         };
     }
 }
