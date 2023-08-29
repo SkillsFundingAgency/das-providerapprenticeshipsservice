eb69178a1362577010ec17903861105e380c1275
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2023-08-29 19:39:22 +0100

    FAI-886 - Sign out redirect fix

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Authentication/AuthenticationExtensions.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Authentication/AuthenticationExtensions.cs
@@ -21,7 +21,7 @@ public static class AuthenticationExtensions
                 typeof(CustomServiceRole),
                 "ProviderRoATP",
                 "/signout",
-                "/");
+                "");
         }
         else
         {
