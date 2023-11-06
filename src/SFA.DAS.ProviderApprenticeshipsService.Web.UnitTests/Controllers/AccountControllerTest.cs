8a42ec97a0481f415b56cd66882223107612bfc3
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-11-06 15:12:59 +0000

    Commit Details:
    Added Back link to the Change of Details Page
    DfESignIn Profile Page link generation based on ResourceEnvironment
    Unit testing added to cover the above changes
    
    Story: https://skillsfundingagency.atlassian.net/browse/FAI-986

---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests/Controllers/AccountControllerTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions.Execution;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SFA.DAS.ProviderApprenticeshipsService.Application.Services.CookieStorageService;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Controllers;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Account;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Models.Types;
+using SFA.DAS.ProviderApprenticeshipsService.Web.Orchestrators;
+
+namespace SFA.DAS.ProviderApprenticeshipsService.Web.UnitTests.Controllers
+{
+    [TestFixture]
+    public class AccountControllerTest
+    {
+        private AccountController _controller = null!;
+        private Mock<ICookieStorageService<FlashMessageViewModel>> _mockCookieStorageService = null!;
+        private Mock<IConfiguration> _mockConfiguration = null!;
+        private Mock<IAccountOrchestrator> _mockAccountOrchestrator = null!;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockConfiguration = new Mock<IConfiguration>();
+            _mockCookieStorageService = new Mock<ICookieStorageService<FlashMessageViewModel>>();
+            _mockAccountOrchestrator = new Mock<IAccountOrchestrator>();
+
+        }
+
+
+        [TestCase("test", "https://test-profile.signin.education.gov.uk/")]
+        [TestCase("preprod", "https://pp-profile.signin.education.gov.uk/")]
+        [TestCase("local", "https://test-profile.signin.education.gov.uk/")]
+        [TestCase("prod", "https://profile.signin.education.gov.uk/")]
+        [TestCase("prd", "https://profile.signin.education.gov.uk/")]
+        public void When_ChangeOfDetails_Then_ViewIsReturned(string env, string profilePageLink)
+        {
+            //arrange
+            _mockConfiguration.Setup(x => x["ResourceEnvironmentName"]).Returns(env);
+            _controller = new AccountController(_mockAccountOrchestrator.Object, _mockCookieStorageService.Object, _mockConfiguration.Object)
+            {
+                ControllerContext = new ControllerContext()
+            };
+            _controller.ControllerContext.HttpContext = new DefaultHttpContext();
+
+            //sut
+            var result = (ViewResult)_controller.ChangeSignInDetails();
+
+            //assert
+            using (new AssertionScope())
+            {
+                result.Should().NotBeNull();
+                var actualModel = result.Model as ChangeOfDetailsViewModel;
+                actualModel.Should().NotBeNull();
+                actualModel?.ProfilePageLink.Should().Be(profilePageLink);
+            }
+        }
+    }
+}
---
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Models/Account/ChangeOfDetailsViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace SFA.DAS.ProviderApprenticeshipsService.Web.Models.Account
+{
+    public class ChangeOfDetailsViewModel
+    {
+        private readonly string _integrationUrlPart;
+
+        public ChangeOfDetailsViewModel(string environment)
+        {
+            switch (!string.IsNullOrEmpty(environment))
+            {
+                case true when environment.Equals("prod", StringComparison.CurrentCultureIgnoreCase):
+                case true when environment.Equals("prd", StringComparison.CurrentCultureIgnoreCase):
+                    _integrationUrlPart = string.Empty;
+                    break;
+                case true when environment.Equals("preprod", StringComparison.CurrentCultureIgnoreCase):
+                    _integrationUrlPart = "pp-";
+                    break;
+                default:
+                    _integrationUrlPart = "test-";
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Gets DfESignIn profile service link.
+        /// </summary>
+        public string ProfilePageLink => $"https://{_integrationUrlPart}profile.signin.education.gov.uk/";
+    }
+}
