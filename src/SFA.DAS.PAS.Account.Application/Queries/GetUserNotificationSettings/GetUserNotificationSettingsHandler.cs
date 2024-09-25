e3d1e22c246e9da21e09ab622c8e16e1493f6f87
Jon Harris <jon.harris@fuitsu.com>
2024-09-25 11:30:52 +0100

    Primary constructor implementation

--- src/SFA.DAS.PAS.Account.Application/Queries/GetUserNotificationSettings/GetUserNotificationSettingsHandler.cs
+++ src/SFA.DAS.PAS.Account.Application/Queries/GetUserNotificationSettings/GetUserNotificationSettingsHandler.cs
@@ -5,33 +5,24 @@ using SFA.DAS.ProviderApprenticeshipsService.Domain.Models.Settings;
 
 namespace SFA.DAS.PAS.Account.Application.Queries.GetUserNotificationSettings;
 
-public class GetUserNotificationSettingsHandler : IRequestHandler<GetUserNotificationSettingsQuery, GetUserNotificationSettingsResponse>
+public class GetUserNotificationSettingsHandler(IUserSettingsRepository userRepository, ILogger<GetUserNotificationSettingsHandler> logger)
+    : IRequestHandler<GetUserNotificationSettingsQuery, GetUserNotificationSettingsResponse>
 {
-    private readonly IUserSettingsRepository _userRepository;
-
-    private readonly ILogger<GetUserNotificationSettingsHandler> _logger;
-
-    public GetUserNotificationSettingsHandler(IUserSettingsRepository userRepository, ILogger<GetUserNotificationSettingsHandler> logger)
-    {
-        _userRepository = userRepository;
-        _logger = logger;
-    }
-
     public async Task<GetUserNotificationSettingsResponse> Handle(GetUserNotificationSettingsQuery message, CancellationToken cancellationToken)
     {
-        var userSettings = (await _userRepository.GetUserSetting(message.UserRef, message.Email)).ToList();
+        var userSettings = (await userRepository.GetUserSetting(message.UserRef, message.Email)).ToList();
         
-        _logger.LogInformation("Attempted to retrieve settings for user {UserRef}", message.UserRef);
+        logger.LogInformation("Attempted to retrieve settings for user {UserRef}", message.UserRef);
 
         if (!userSettings.Any())
         {
-            _logger.LogInformation("No settings found. Creating user settings for userRef {UserRef}", message.UserRef);
+            logger.LogInformation("No settings found. Creating user settings for userRef {UserRef}", message.UserRef);
 
-            await _userRepository.AddSettings(message.Email);
+            await userRepository.AddSettings(message.Email);
             
-            userSettings = (await _userRepository.GetUserSetting(message.UserRef, message.Email)).ToList();
+            userSettings = (await userRepository.GetUserSetting(message.UserRef, message.Email)).ToList();
 
-            _logger.LogInformation("Created default settings for user {UserRef}", message.UserRef);
+            logger.LogInformation("Created default settings for user {UserRef}", message.UserRef);
         }
 
         return new GetUserNotificationSettingsResponse
