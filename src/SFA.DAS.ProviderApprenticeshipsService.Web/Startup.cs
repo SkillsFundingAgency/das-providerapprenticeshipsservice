afbfc3ef898636669d93101a25c0af9f55124810
Jon Harris <jon.harris@fuitsu.com>
2024-11-12 07:23:45 +0000

    Using OpenTelemetry instead of App Insights package

--- src/SFA.DAS.ProviderApprenticeshipsService.Web/Startup.cs
+++ src/SFA.DAS.ProviderApprenticeshipsService.Web/Startup.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging.ApplicationInsights;
 using NServiceBus.ObjectBuilder.MSDependencyInjection;
+using OpenTelemetry.Logs;
 using SFA.DAS.NServiceBus.Features.ClientOutbox.Data;
 using SFA.DAS.Provider.Shared.UI;
 using SFA.DAS.Provider.Shared.UI.Startup;
@@ -10,7 +10,6 @@ using SFA.DAS.ProviderApprenticeshipsService.Application.Extensions;
 using SFA.DAS.ProviderApprenticeshipsService.Application.RegistrationExtensions;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Attributes;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Authentication;
-using SFA.DAS.ProviderApprenticeshipsService.Web.Authorization;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Exceptions;
 using SFA.DAS.ProviderApprenticeshipsService.Web.Extensions;
 using SFA.DAS.ProviderApprenticeshipsService.Web.ServiceRegistrations;
@@ -38,8 +37,8 @@ public class Startup
 
         services.AddLogging(builder =>
         {
-            builder.AddFilter<ApplicationInsightsLoggerProvider>(string.Empty, LogLevel.Information);
-            builder.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", LogLevel.Information);
+            builder.AddFilter<OpenTelemetryLoggerProvider>(string.Empty, LogLevel.Information);
+            builder.AddFilter<OpenTelemetryLoggerProvider>("Microsoft", LogLevel.Information);
         });
 
         services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<DeleteRegisteredUserCommand>());
@@ -76,10 +75,10 @@ public class Startup
             .SetDfESignInConfiguration(true)
             .EnableCookieBanner()
             .SetDefaultNavigationSection(NavigationSection.Home);
-        
+
         services.AddDataProtection(_configuration);
 
-        services.AddApplicationInsightsTelemetry();
+        services.AddOpenTelemetryRegistration(_configuration);
     }
 
     public void ConfigureContainer(UpdateableServiceProvider serviceProvider)
