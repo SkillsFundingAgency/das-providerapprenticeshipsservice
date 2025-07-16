02436640cc52d5b692769d69056ce415259caca5
Deven Shah <devenshah1974@gmail.com>
2025-07-16 09:34:37 +0100

    CSP-1954 Delete provider agreements webjob

--- src/SFA.DAS.PAS.ContractAgreements.WebJob/Configuration/ConfigurationKeys.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace SFA.DAS.PAS.ContractAgreements.WebJob.Configuration;
-
-public static class ConfigurationKeys
-{
-    public const string ContractAgreements = "SFA.DAS.ContractAgreements";
-}
--- src/SFA.DAS.PAS.ContractAgreements.WebJob/ContractFeed/IContractFeedEventValidator.cs
+++
@@ -1,8 +0,0 @@
-using SFA.DAS.ProviderApprenticeshipsService.Domain.ContractFeed;
-
-namespace SFA.DAS.PAS.ContractAgreements.WebJob.ContractFeed;
-
-public interface IContractFeedEventValidator
-{
-    bool Validate(ContractFeedEvent contractFeedEvent);
-}
