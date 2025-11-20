3b1a36aa33786b81da44c18de356217fe471107b
Deven Shah <devenshah1974@gmail.com>
2025-11-20 14:47:11 +0000

    CSP-1954 Deleted the tables

---
+++ src/SFA.DAS.ProviderAgreementStatus.Database/Script.PostDeployment.sql
@@ -0,0 +1,10 @@
+﻿IF OBJECT_ID(N'dbo.ContractFeedEvent', N'U') IS NOT NULL  
+   DROP TABLE [dbo].[ContractFeedEvent];
+
+GO
+
+IF OBJECT_ID(N'dbo.ContractFeedEventRun', N'U') IS NOT NULL  
+   DROP TABLE [dbo].[ContractFeedEventRun];
+
+GO
+
