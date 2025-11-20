1666f7db3671b0cead721f37341135d77016f86c
Deven Shah <devenshah1974@gmail.com>
2025-11-20 12:09:22 +0000

    CSP-1954 Deleted provider agreement endpoint

--- src/SFA.DAS.PAS.Account.Application/Queries/GetProviderAgreement/GetProviderAgreementQueryRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediatR;
-
-namespace SFA.DAS.PAS.Account.Application.Queries.GetProviderAgreement;
-
-public class GetProviderAgreementQueryRequest : IRequest<GetProviderAgreementQueryResponse>
-{
-    public long ProviderId { get; set; }
-}
--- src/SFA.DAS.PAS.Account.Application/Queries/GetProviderAgreement/GetProviderAgreementQueryResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿using SFA.DAS.ProviderApprenticeshipsService.Domain.Enums;
-
-namespace SFA.DAS.PAS.Account.Application.Queries.GetProviderAgreement;
-
-public class GetProviderAgreementQueryResponse
-{
-    public ProviderAgreementStatus HasAgreement { get; set; }
-}
